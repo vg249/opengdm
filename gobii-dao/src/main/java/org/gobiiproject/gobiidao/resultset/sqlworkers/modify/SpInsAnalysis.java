85b2eb41626986eb5157d26c1ca365c464d86054
Philip Glaser <phil.glaser@gmail.com>
2016-04-26 09:26:50 -0400

    Started on create Analysis SP Configuration changes for server deployment

---
+++ gobii-dao/src/main/java/org/gobiiproject/gobiidao/resultset/sqlworkers/modify/SpInsAnalysis.java
@@ -0,0 +1,34 @@
+package org.gobiiproject.gobiidao.resultset.sqlworkers.modify;
+
+import org.gobiiproject.gobiidao.resultset.core.SpDef;
+
+import java.sql.Date;
+import java.util.List;
+
+/**
+ * Created by Phil on 4/7/2016.
+ */
+public class SpInsAnalysis extends SpDef {
+
+    public SpInsAnalysis() {
+
+        super("{call createanalysis(?,?,?,?,?,?,?,?,?,?,?,?,?)}");
+
+//        this.addParamDef("experimentid", Integer.class).setNullable(false);
+//        this.addParamDef("callinganalysisid", Integer.class).setNullable(false);
+//        this.addParamDef("datasetanalyses", List.class).setNullable(true);
+//        this.addParamDef("datatable", String.class).setNullable(false);
+//        this.addParamDef("datafile", String.class).setNullable(false);
+//        this.addParamDef("qualitytable", String.class).setNullable(true);
+//        this.addParamDef("qualityfile", String.class).setNullable(true);
+//        this.addParamDef("datasetscores", String.class).setNullable(true); //jsonb!!!
+//        this.addParamDef("createdby", Integer.class).setNullable(true);
+//        this.addParamDef("createdDate.class", Date.class).setNullable(true);
+//        this.addParamDef("modifiedby", Integer.class).setNullable(true);
+//        this.addParamDef("modifiedDate.class", Date.class).setNullable(true);
+//        this.addParamDef("datasetstatus", Integer.class).setNullable(false);
+
+
+    } // ctor
+
+}
