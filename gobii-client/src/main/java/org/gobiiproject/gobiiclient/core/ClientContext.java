60f4930837d6721dd726bde8795e0a3bf1cc4ad5
Philip Glaser <phil.glaser@gmail.com>
2016-05-13 10:12:46 -0400

    * client context in progress; * refactor out typed rest functionality

---
+++ gobii-client/src/main/java/org/gobiiproject/gobiiclient/core/ClientContext.java
@@ -0,0 +1,67 @@
+package org.gobiiproject.gobiiclient.core;
+
+import org.gobiiproject.gobiimodel.ConfigSettings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Phil on 5/13/2016.
+ */
+public final class ClientContext {
+
+
+    // configure as a singleton
+    // this may not be effective if more thn one classloader is used
+    public final static ClientContext INSTANCE = new ClientContext();
+
+    private void ClientContext() throws Exception {
+
+        configSettings = new ConfigSettings();
+
+        for(ConfigSettings.CropType currentCropType : ConfigSettings.CropType.values()) {
+            configSettings.setCurrentCropType(currentCropType);
+            String currentCropDomain = configSettings.getCurrentCropConfig().getServiceDomain();
+            String currentCropPort = configSettings.getCurrentCropConfig().getServicePort().toString();
+            String currentBaseUrl = currentCropDomain + ":" + currentCropPort + "\\";
+            baseUrlsByCrop.put(currentCropType,currentBaseUrl);
+
+        }
+    }
+
+    public enum ProcessMode {Asynch, Block}
+
+    private Map<ConfigSettings.CropType,String> baseUrlsByCrop = new HashMap<>();
+    private ProcessMode processMode = ProcessMode.Asynch;
+    private String userToken = null;
+    private ConfigSettings configSettings;
+    private ConfigSettings.CropType currentClientCrop = ConfigSettings.CropType.RICE;
+
+
+    public String getBaseUrl(ConfigSettings.CropType cropType) {
+        return  baseUrlsByCrop.get(cropType);
+    }
+
+    public List<ConfigSettings.CropType> getCropTypeTypes() {
+        return new ArrayList<>(Arrays.asList(ConfigSettings.CropType.values()));
+    }
+
+
+    public ConfigSettings.CropType getCurrentClientCrop() {
+        return currentClientCrop;
+    }
+
+    public void setCurrentClientCrop(ConfigSettings.CropType currentClientCrop) {
+        this.currentClientCrop = currentClientCrop;
+    }
+
+//    login(String userName, String password ) {
+//        RestRequest
+//        String token = restRequest.getTokenForUser(userDetail.getUserName(), userDetail.getPassword());
+//
+//    }
+
+}
--- gobii-client/src/main/java/org/gobiiproject/gobiiclient/core/RestRequest.java
+++ gobii-client/src/main/java/org/gobiiproject/gobiiclient/core/RestRequest.java
@@ -31,216 +31,20 @@ import java.net.URI;
 public class RestRequest<T> {
 
 
-
     private final Class<T> paramType;
-    private String host = null;
-    private Integer port = null;
+    private HttpCore httpCore = new HttpCore();
+    Logger LOGGER = LoggerFactory.getLogger(RestRequest.class);
 
     @SuppressWarnings("unchecked")
     public RestRequest(Class<T> paramType) {
 
         this.paramType = paramType;
 
-        try {
-            ConfigSettings configSettings = new ConfigSettings();
-
-            String host = configSettings.getCurrentCropConfig().getServiceDomain();
-            if( null != host ) {
-                this.host = host;
-            } else {
-                this.host = "localhost";
-                LOGGER.info("servicedomain property is not specified in configuration; setting domain to localhost");
-            }
-
-            Integer port = configSettings.getCurrentCropConfig().getServicePort();
-            if( null != port ) {
-                this.port = port;
-            } else {
-                this.port = 8080;
-                LOGGER.info("serviceport property is not specified in configuration; setting port to 8080");
-            }
-
-
-        } catch( Exception e) {
-            LOGGER.error(e.getMessage());
-        }
     } // ctor
 
-    Logger LOGGER = LoggerFactory.getLogger(RestRequest.class);
-
-
-    private static final String HEADER_TOKEN = "X-Auth-Token";
-    private static final String HEADER_USERNAME = "X-Username";
-    private static final String HEADER_PASSWORD = "X-Password";
-
-
-    final private static int HTTP_STATUS_CODE_OK = 200;
-    final private static int HTTP_STATUS_CODE_UNAUTHORIZED = 401;  //not authenticated
-    final private static int HTTP_STATUS_CODE_FORBIDDEN = 403; //not authorized
-
-    private URIBuilder getBaseBuilder() throws Exception {
-        return (new URIBuilder().setScheme("http")
-                .setHost(host)
-                .setPort(port));
-    }
-
-    private URI makeUri(String url) throws Exception {
-
-        URI returnVal = getBaseBuilder()
-                .setPath(url)
-                .build();
-
-        return (returnVal);
-
-    }//byContentTypeUri()
-
-    private URI logoutURI() throws Exception {
-
-        URI returnVal = getBaseBuilder()
-                .setPath("/logout") //configured in security configuration xml
-                .build();
-
-        return (returnVal);
-    }//byContentTypeUri()
-
-    private HttpResponse submitUriRequest(HttpUriRequest httpUriRequest, String userName, String password, String token) throws Exception {
-        httpUriRequest.addHeader("Content-Type", "application/json");
-        httpUriRequest.addHeader("Accept", "application/json");
-
-        if ((null != token) && (false == token.isEmpty())) {
-            httpUriRequest.addHeader(HEADER_TOKEN, token);
-        } else {
-            httpUriRequest.addHeader(HEADER_USERNAME, userName);
-            httpUriRequest.addHeader(HEADER_PASSWORD, password);
-        }
-
-        return (HttpClientBuilder.create().build().execute(httpUriRequest));
-
-    }// submitUriRequest()
-
-
-    private void logInfo(String logMessage) {
-        LOGGER.info(logMessage);
-    } // logInfo() 
-
-    private void logHeaders(Header[] headers) {
-
-        for (Header currentHeader : headers) {
-            logInfo(currentHeader.getName() + ": " + currentHeader.getValue());
-        }
-
-    }//logHeaders()
-
-    private Header getHeader(Header[] headers, String headerName) {
-        Header returnVal = null;
-
-        boolean foundHeader = false;
-        for (int idx = 0; idx < headers.length && foundHeader == false; idx++) {
-            Header currentHeader = headers[idx];
-
-            if (headerName.equals(currentHeader.getName())) {
-                returnVal = currentHeader;
-                foundHeader = true;
-            }
-        }//iterate headers
-
-        return (returnVal);
-
-    }//getHeader()
-
-    private void logRequestHeaders(HttpUriRequest httpUriRequest, HttpResponse httpResponse, String testName) throws Exception {
-
-        
-
-        logInfo("============================================ BEGIN TEST " + testName + "==================================");
-        logInfo("");
-        logInfo("");
-        logInfo("");
-        logInfo("****************");
-        logInfo("****************");
-        logInfo("****************** Request URL");
-        logInfo(httpUriRequest.getURI().toString());
-
-
-        logInfo("");
-        logInfo("");
-        logInfo("");
-        logInfo("****************");
-        logInfo("****************");
-        logInfo("****************** Request Headers");
-        logHeaders(httpUriRequest.getAllHeaders());
-
-        logInfo("");
-        logInfo("");
-        logInfo("");
-        logInfo("****************");
-        logInfo("****************");
-        logInfo("****************** Response  Headers");
-        logHeaders(httpResponse.getAllHeaders());
-
-        logInfo("");
-        logInfo("");
-        logInfo("");
-        logInfo("****************");
-        logInfo("****************");
-        logInfo("****************** Response  Status");
-        logInfo(httpResponse.getStatusLine().toString());
-
-        logInfo("");
-        logInfo("");
-        logInfo("");
-        BufferedReader bufferedReader = new BufferedReader(
-                new InputStreamReader((httpResponse.getEntity().getContent())));
-
-        String output;
-        logInfo("****************");
-        logInfo("****************");
-        logInfo("********** *******Response Body content");
-        while ((output = bufferedReader.readLine()) != null) {
-            logInfo(output);
-        }
-
-        logInfo("============================================ END  TEST " + testName + "==================================");
-
-
-    }//logRequestHeaders()
-
-    private HttpResponse authenticateWithUser(String userName, String password) throws Exception {
-
-        HttpResponse returnVal = null;
-
-        URI uri = makeUri(Urls.URL_AUTH);
-        HttpPost postRequest = new HttpPost(uri);
-        returnVal = submitUriRequest(postRequest, userName, password, null);
-
-        if( HTTP_STATUS_CODE_OK !=  returnVal.getStatusLine().getStatusCode()) {
-            throw new Exception( "Request did not succeed: " + returnVal.getStatusLine().getStatusCode()  );
-        }
-
-
-        // logRequestHeaders(postRequest, returnVal, " Authenticate with user " + userName);
-
-        return (returnVal);
-
-    }//authenticateWithUser()
-
     public String getTokenForUser(String userName, String password) throws Exception {
-
-        String returnVal = null;
-
-        HttpResponse response = authenticateWithUser(userName, password);
-        Header tokenHeader = getHeader(response.getAllHeaders(), HEADER_TOKEN);
-        returnVal = tokenHeader.getValue();
-
-        if (null == returnVal) {
-            LOGGER.error("Unable to get authentication token for user " + userName);
-        }
-
-        return returnVal;
-
-    } // getTokenForUser()
-
-
+        return httpCore.getTokenForUser(userName, password);
+    }
 
     public T getTypedHtppResponseForDto(String url,
                                         T dtoInstance,
@@ -251,7 +55,7 @@ public class RestRequest<T> {
 
         ObjectMapper objectMapper = new ObjectMapper();
         String dtoRequestJson = objectMapper.writeValueAsString(dtoInstance);
-        JsonObject responseJson = getResponseBody(url, dtoRequestJson, token);
+        JsonObject responseJson = httpCore.getResponseBody(url, dtoRequestJson, token);
 
         returnVal = objectMapper.readValue(responseJson.toString(), paramType);
 
@@ -261,14 +65,13 @@ public class RestRequest<T> {
     } // getTypedHtppResponseForDto()
 
 
-
     public T getTypedHtppResponse(String url,
                                   JsonObject requestJson,
                                   String token) throws Exception {
 
         T returnVal = null;
 
-        JsonObject responseJson = getResponseBody(url, requestJson.toString(), token);
+        JsonObject responseJson = httpCore.getResponseBody(url, requestJson.toString(), token);
 
         ObjectMapper objectMapper = new ObjectMapper();
         returnVal = objectMapper.readValue(responseJson.toString(), paramType);
@@ -276,47 +79,5 @@ public class RestRequest<T> {
         return returnVal;
 
     } // getTypedHtppResponse()
-
-
-    public JsonObject getResponseBody(String url,
-                                      String jsonString,
-                                      String token) throws Exception {
-
-        JsonObject returnVal = null;
-
-        HttpResponse httpResponse = null;
-
-        URI uri = makeUri(url);
-
-        HttpPost postRequest = new HttpPost(uri);
-//        String jsonString = jsonObject.toString();
-        StringEntity input = new StringEntity(jsonString);
-        postRequest.setEntity(input);
-
-        httpResponse = submitUriRequest(postRequest, "", "", token);
-
-        if( HTTP_STATUS_CODE_OK !=  httpResponse.getStatusLine().getStatusCode()) {
-            throw new Exception( "Request did not succeed: " + httpResponse.getStatusLine().getStatusCode()  );
-        }
-
-
-        BufferedReader bufferedReader = new BufferedReader(
-                new InputStreamReader((httpResponse.getEntity().getContent())));
-
-        StringBuilder stringBuilder = new StringBuilder();
-        String currentLine = null;
-        while ((currentLine = bufferedReader.readLine()) != null) {
-            stringBuilder.append(currentLine);
-        }
-
-
-        JsonParser parser = new JsonParser();
-        String jsonAsString = stringBuilder.toString();
-        returnVal = parser.parse(jsonAsString).getAsJsonObject();
-
-        return returnVal;
-
-    }//accessResource_test
-
 
 }// ArgumentDAOTest
