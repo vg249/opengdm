76d015a9b9a94f1d70b379272e592f1d079ba77d
Philip Glaser <phil.glaser@gmail.com>
2016-05-23 07:53:59 -0400

    Add http to service

--- gobii-web/src/main/ts/services/app/name-id-list.service.ts
+++ gobii-web/src/main/ts/services/app/name-id-list.service.ts
@@ -1,31 +1,46 @@
-import {Injectable, Component} from 'angular2/core';
+import {Injectable} from 'angular2/core';
 
 import {NameId} from '../../model/name-id';
-import { Http, Response } from 'angular2/http';
-import { Observable }     from 'rxjs/Observable';
+import {Http, Response} from 'angular2/http';
+import {Observable}     from 'rxjs/Observable';
 
 @Injectable()
 export class NameIdListService {
 
 
-	constructor (private _http : Http ) {}
-	
-	getNameIds() {
-		
-		let requestBody = {
-			   "processType":"READ",
-			   "entityType":"DBTABLE",
-			   "entityName":"project",
-			   "filter":"2"
-			};
+    constructor(private _http:Http) {
+    }
 
-		var nameIds: NameId[];
-		
+    public getNameIds():Observable<NameId[]> {
 
+        let requestBody = `
+            "processType": "READ",
+            "entityType": "DBTABLE",
+            "entityName": "project",
+            "filter": "2"        
+            `;
 
-		
-					
-		return nameIds;
-	}
+        return this
+            ._http
+            .post("extract/nameidlist", requestBody)
+            .map(this.handleResponse)
+            .catch(this.handleError);
+    }
 
+    private handleResponse(response:Response) {
+
+        if (response.status < 200 || response.status > 300) {
+            throw new Error('Bad response status: ' + response.status);
+        }
+
+        let payload = response.json;
+        console.log(payload);
+        return [];
+    }
+
+    private handleError(error:any) {
+        let errorMessage = error.message;
+        console.error(errorMessage);
+        return Observable.throw(errorMessage);
+    }
 } 
