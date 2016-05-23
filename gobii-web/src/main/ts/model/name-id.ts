31a692492398c2a892f5e3314d3fb6fe60591275
Philip Glaser <phil.glaser@gmail.com>
2016-05-23 15:15:52 -0400

    Works except debugging

--- gobii-web/src/main/ts/model/name-id.ts
+++
@@ -1,8 +0,0 @@
-export class NameId {
-	
-		constructor(public id, public name) {
-			
-			this.id = id;
-			this.name = name; 
-    }
-}
--- gobii-web/src/main/ts/root/main.ts
+++
@@ -1,4 +0,0 @@
-import {bootstrap}    from 'angular2/platform/browser'
-import {ExtractorRoot} from './app.extractorroot'
-bootstrap(ExtractorRoot);
-
