07cda557c566e9f30d1b3b203ba33d977a7c0c88
Philip Glaser <phil.glaser@gmail.com>
2016-06-15 14:51:30 -0400

    update contact: use correct number of parameters modify for change in parameter order

--- gobii-dao/src/main/java/org/gobiiproject/gobiidao/resultset/sqlworkers/modify/SpUpdContact.java
+++ gobii-dao/src/main/java/org/gobiiproject/gobiidao/resultset/sqlworkers/modify/SpUpdContact.java
@@ -11,7 +11,7 @@ public class SpUpdContact extends SpDef {
 
     public SpUpdContact() {
 
-        super("{call updatecontact(?,?,?,?,?,?,?,?,?,?,?,?)}",false);
+        super("{call updatecontact(?,?,?,?,?,?,?,?,?,?,?)}",false);
 
         this.addParamDef("contactId", Integer.class).setNullable(false);
         this.addParamDef("lastName", String.class).setNullable(false);
@@ -22,8 +22,8 @@ public class SpUpdContact extends SpDef {
         this.addParamDef("createdBy", Integer.class).setNullable(false);
         this.addParamDef("createdDate", java.util.Date.class).setNullable(false);
         this.addParamDef("modifiedBy", Integer.class).setNullable(true);
-        this.addParamDef("organizationId", Integer.class).setNullable(true);
         this.addParamDef("modifiedDate", java.util.Date.class).setNullable(true);
+        this.addParamDef("organizationId", Integer.class).setNullable(true);
 
 
     } // ctor
