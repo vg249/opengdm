f088d6c6e822fe4ce970f6ff93aaf4c911931dd9
Phil Glaser <pdg66@cornell.edu>
2016-09-01 18:03:53 -0400

    Added new meta level properties

---
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/dto/instructions/GobiiFilePropNameId.java
@@ -0,0 +1,25 @@
+package org.gobiiproject.gobiimodel.dto.instructions;
+
+/**
+ * Created by Phil on 9/1/2016.
+ */
+public class GobiiFilePropNameId {
+    Integer Id;
+    String name;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
