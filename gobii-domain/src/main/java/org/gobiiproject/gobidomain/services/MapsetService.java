ff5773b9727ca1c553a50c3d172fe569bee7759b
av484 <av484@cornell.edu>
2016-09-29 14:20:40 -0400

    A minor change for the first test in according to the document "Refactoring Legacy Web Services"

--- gobii-domain/src/main/java/org/gobiiproject/gobidomain/services/MapsetService.java
+++ gobii-domain/src/main/java/org/gobiiproject/gobidomain/services/MapsetService.java
@@ -1,17 +1,18 @@
 package org.gobiiproject.gobidomain.services;
 
-import org.gobiiproject.gobidomain.GobiiDomainException;
-import org.gobiiproject.gobiimodel.headerlesscontainer.MapsetDTO;
-
 import java.util.List;
+
+import org.gobiiproject.gobidomain.GobiiDomainException;
+import org.gobiiproject.gobiimodel.dto.container.MapsetDTO;
 
 /**
  * Created by Phil on 4/28/2016.
+ * Modified by AVB on 9/29/2016.
  */
 public interface MapsetService {
 
-    //MapsetDTO processMapset(MapsetDTO MapsetDTO);
+    MapsetDTO processMapset(MapsetDTO MapsetDTO);
 
-    List<MapsetDTO> getAllMapsetNames ()  throws GobiiDomainException;
+    List<MapsetDTO> getAllMapsetNames() throws GobiiDomainException;
 
 }
