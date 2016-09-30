0cd28d8eae425862f91041ee968e715dd56affd5
av484 <av484@cornell.edu>
2016-09-30 14:09:54 -0400

    A minor change for the first test in according to the document "Refactoring Legacy Web Services"

--- gobii-dtomapping/src/main/java/org/gobiiproject/gobiidtomapping/DtoMapMapset.java
+++ gobii-dtomapping/src/main/java/org/gobiiproject/gobiidtomapping/DtoMapMapset.java
@@ -1,10 +1,12 @@
 package org.gobiiproject.gobiidtomapping;
 
 import java.util.List;
+
 import org.gobiiproject.gobiimodel.dto.container.MapsetDTO;
 
 /**
  * Created by Phil on 4/28/2016.
+ * Modified by AVB on 9/30/2016.
  */
 public interface DtoMapMapset {
 
@@ -12,4 +14,5 @@ public interface DtoMapMapset {
     MapsetDTO getMapsetDetails(MapsetDTO mapsetDTO) throws GobiiDtoMappingException;
     MapsetDTO createMapset(MapsetDTO mapsetDTO) throws GobiiDtoMappingException;
     MapsetDTO updateMapset(MapsetDTO mapsetDTO) throws GobiiDtoMappingException;
+
 }
