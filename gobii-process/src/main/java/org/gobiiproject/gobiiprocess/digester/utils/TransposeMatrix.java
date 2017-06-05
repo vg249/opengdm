55dcb51fd6663e9d95417f21262517e403fa4f01
Joshua Lamos-Sweeney <jdl232@cornell.edu>
2017-06-05 17:09:07 -0400

    Basic multithread implementation in TransposeMatrix. Also, file deletion from Java directly, using the same method we get the input files. Some cleanup of error messages, and removal of a System.exit() that would have ruined someone's day.

--- gobii-process/src/main/java/org/gobiiproject/gobiiprocess/digester/utils/TransposeMatrix.java
+++ gobii-process/src/main/java/org/gobiiproject/gobiiprocess/digester/utils/TransposeMatrix.java
@@ -3,19 +3,20 @@ package org.gobiiproject.gobiiprocess.digester.utils;
 import org.apache.commons.lang.StringUtils;
 import org.gobiiproject.gobiimodel.utils.error.ErrorLogger;
 import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.gobiiproject.gobiimodel.utils.FileSystemInterface.lineCount;
 import static org.gobiiproject.gobiimodel.utils.HelperFunctions.checkFileExistence;
 import static org.gobiiproject.gobiimodel.utils.HelperFunctions.tryExec;
+import static org.gobiiproject.gobiiprocess.digester.utils.TransposeMatrix.transposeMatrix;
 
 public class TransposeMatrix {
     private static long startTime, endTime, duration;
-    public static boolean transposeMatrix (String sep, String iFile, String oFile, String dest) throws FileNotFoundException{
+    public static void transposeMatrix (String sep, String iFile, String oFile, String dest) throws FileNotFoundException{
         if (!checkFileExistence(iFile)) {
-            ErrorLogger.logError("Transpose Matrix", "Input file provided does not exists.\n");
-            throw new FileNotFoundException("IUPAC to Bi:\t" + iFile +"does not exists");
+            ErrorLogger.logError("Transpose Matrix", "Input file "+iFile+" provided does not exists.\n");
         }
-        boolean success;
         startTime = System.currentTimeMillis();
 
         switch (sep){
@@ -32,17 +33,36 @@ public class TransposeMatrix {
         tryExec("split "+iFile+" "+dest+"/_transpose_");
         File directory = new File(dest);
         File files[] = directory.listFiles();
-        String outFile;
+        List<Thread> threads=new LinkedList<>();
+
         for (File inFile: files){
             if(inFile.getName().matches("_transpose_.*")){
-                outFile = inFile.toString().replace("_transpose_","_transposed_");
-                //TODO: Multi threading for the below line.
-                transposeMatrix(sep, inFile.toString(), outFile);
+                String outFile = inFile.toString().replace("_transpose_","_transposed_");
+                Thread transposeThread=new Thread(new TransposeThread(sep,inFile,outFile));
+                transposeThread.start();
+                threads.add(transposeThread);
             }
         }
-        success=tryExec("paste "+dest+"/_transposed_*", oFile, null);
-        success&=tryExec("rm -f "+dest+"/_trans*"); // dangerous
-        return success;
+
+        //Wait for all threads to complete, lets just wait for all of them in order (obviously, many will be done
+        // before hand, in which case 'join' does nothing. Brilliant.
+        for(Thread t:threads){
+            try {
+                t.join();
+            }
+            catch(InterruptedException e){
+                ErrorLogger.logError("TransposeMatrix","Interrupt",e);
+            }
+        }
+
+        tryExec("paste "+dest+"/_transposed_*", oFile, null);
+
+        //Delete all intermediates once 'paste' is complete
+        for (File inFile: files){
+            if(inFile.getName().matches("_transposed_.*")){
+                inFile.delete();
+            }
+        }
     }
 
     /***
@@ -52,14 +72,13 @@ public class TransposeMatrix {
      * @param oFile = Output File
      * @return success
      */
-    private static boolean transposeMatrix(String sep, String iFile, String oFile) {
+    protected static boolean transposeMatrix(String sep, String iFile, String oFile){
         int lineNumber = lineCount(iFile);
         int lineNo=0, colNo=0;
         String inLine;
         int columnNumber = getColumnNumber(iFile, sep);
         if(columnNumber < 0){
             ErrorLogger.logError("Transpose Matrix", "Input file does not contain any content.\n");
-            System.exit(1);
         }
         String[][] Matrix = new String[columnNumber][lineNumber];
         try(BufferedWriter buffOut=new BufferedWriter(new FileWriter(oFile));
@@ -79,12 +98,12 @@ public class TransposeMatrix {
             }
             endTime = System.currentTimeMillis();
             duration = endTime-startTime;
-            ErrorLogger.logTrace("Transpose Matrix","Time taken" + "(" + iFile + "):" + duration/1000 + " Secs");
+            ErrorLogger.logTrace("Transpose Matrix","Time taken" + "(" + iFile + "):" + duration/1000 + " Seconds");
             return true;
         } catch (FileNotFoundException e){
-            ErrorLogger.logError("Transpose Matrix","Missing output File:", e);
+            ErrorLogger.logError("Transpose Matrix","Missing file", e);
         } catch (IOException e){
-            ErrorLogger.logError("Transpose Matrix","Unexpected error", e);
+            ErrorLogger.logError("Transpose Matrix","IOException transposing from "+iFile+" to "+oFile, e);
         }
         return false;
     }
@@ -105,6 +124,27 @@ public class TransposeMatrix {
         }
     }
 
+}
+
+class TransposeThread implements Runnable{
+    String sep,oFile;
+    File iFile;
+    protected TransposeThread(String sep, File iFile, String oFile){
+        this.sep=sep;
+        this.iFile=iFile;
+        this.oFile=oFile;
+    }
+    @Override
+    public void run() {
+        try {
+            transposeMatrix(sep,iFile.toString(),oFile);
+            //Clean up input file when done
+            iFile.delete();
+        }
+        catch(Exception e){
+            ErrorLogger.logError("TransposeThread","Error processing short file transposition",e);
+        }
+    }
 }
 
 
