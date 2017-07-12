e257f13891a0c331ecaf1943192fe1198cbc7fd1
Phil Glaser <pdg66@cornell.edu>
2017-07-12 08:22:21 -0400

    Set qc output folder with official enum

--- gobii-dtomapping/src/main/java/org/gobiiproject/gobiidtomapping/impl/DtoMapExtractorInstructionsImpl.java
+++ gobii-dtomapping/src/main/java/org/gobiiproject/gobiidtomapping/impl/DtoMapExtractorInstructionsImpl.java
@@ -203,7 +203,14 @@ public class DtoMapExtractorInstructionsImpl implements DtoMapExtractorInstructi
                                         + currentGobiiDataSetExtract.getGobiiExtractFilterType());
                     }
 
-                    String extractionFileDestinationPath = configSettings.getProcessingPath(cropType, GobiiFileProcessDir.EXTRACTOR_OUTPUT);
+                    String extractionFileDestinationPath;
+
+                    if(!currentExtractorInstruction.isQcCheck()) {
+                        extractionFileDestinationPath = configSettings.getProcessingPath(cropType, GobiiFileProcessDir.EXTRACTOR_OUTPUT);
+                    } else {
+                        extractionFileDestinationPath = configSettings.getProcessingPath(cropType,GobiiFileProcessDir.QC_OUTPUT);
+                    }
+
                     extractorFileDestinationLocation = this.makeDestinationDirectoryName(currentExtractorInstruction.getContactEmail(),
                             currentGobiiDataSetExtract.getGobiiExtractFilterType(),
                             currentGobiiDataSetExtract.getGobiiFileType(),
@@ -214,9 +221,6 @@ public class DtoMapExtractorInstructionsImpl implements DtoMapExtractorInstructi
                         extractorFileDestinationLocation += "/" + idx.toString();
                     }
 
-                    if(currentExtractorInstruction.isQcCheck()) {
-                        extractorFileDestinationLocation = currentGobiiDataSetExtract.getExtractDestinationDirectory();
-                    }
 
                     if (!instructionFileAccess.doesPathExist(extractorFileDestinationLocation)) {
 
