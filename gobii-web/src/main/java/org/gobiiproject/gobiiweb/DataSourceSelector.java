8f9e353c6469e0057026a908fbfc40dbf56630ba
Phil Glaser <pdg66@cornell.edu>
2017-10-08 08:42:46 -0400

    add more detail to the comment

--- gobii-web/src/main/java/org/gobiiproject/gobiiweb/DataSourceSelector.java
+++ gobii-web/src/main/java/org/gobiiproject/gobiiweb/DataSourceSelector.java
@@ -14,17 +14,37 @@ public class DataSourceSelector extends AbstractRoutingDataSource {
     Logger LOGGER = LoggerFactory.getLogger(DataSourceSelector.class);
 
     /*
-    For reasons that are not clear, for the very first request that comes in,
-    the ServletRequest is not available through the ServletRequestAttributes.
+    For reasons that are not clear, for the very first time that the determineCurrentLookupkey()
+    method is hit, the ServletRequest is not available through the ServletRequestAttributes
+    (this is the mechanism used by the CropRequestAnalyzer to figure out which crop
+    to use based on the inbound url). This happens because in the TokenAuthenticationFilter
+    we need to look up the user in the contact table as part of the authentication
+    process. I gather that the nominal use case for setting up JDBC connections
+    is that they are defined statically, such that a given applicaiton would just
+    always be directed towards a specific connection through. In our case, we identify
+    the connection dynamically. Apparently, it is ordinarily not until the delegatingProxyFilterChain
+    has completed that the ServeltRequestAttributes are set up.
     In this scenario, the CroRequestAnalyzer was defaulting to provide the
     ID of the first active gobii server instance listed in the config file.
-    For single crop configurations this didn't matter. But when more than
-    one crop was configured, it meant that for the authentication request --
-    and only for that request before the request context was establsihed --
+    For single crop configurations this doesn't matter. But when more than
+    one crop is configured, it means that for the authentication request --
+    and only for that request before the ServlettRequestAttributes are set up  --
     the user credentials were being retrieved from the wrong database. Accordingly
-    I set up a oncePerRequest filter that sets the servlet request in a thread local.
-    I will need to research whether this is really a correct long term solution.
-    But it seems to work fine.
+    I set up a oncePerRequest filter that sets the servlet request in a thread local that is
+    then visible to this class, which can then use the method of CropRequestAnalyzer to
+    which you provide the request object directly.
+    One thing to note is that when you look at this problem in the debugger, it looks
+    as if there is only one initial request in an entire series of requests in which
+    the ServletRequestAttributes are not set. However, this is an illusion: once you've
+    authenticated, the lookup in the contact table is not done because the you have a
+    token. In other words, once you've authenticated and have a token, the branch
+    of TokenAuthenticationFilter that looks up the user in the contact table does
+    not execute. Thus, it is _only_ for the request in which the user's identity
+    is established -- i.e., in which the user authenticates -- that the empty
+    ServletRequestAttributes are a problem.
+    Perhaps we should be doing static configuration instead to avoid this complication.
+    However, this approach makes our server configurations much easier: it makes it
+    unnecessary to manage static configuration for each specific crop type.
      */
     private ThreadLocal<HttpServletRequest> currentRequest;
     public ThreadLocal<HttpServletRequest> getCurrentRequest() {
