26ae1a5770f1479fe6b4fb4045f1ee68d1195355
Phil Glaser <pdg66@cornell.edu>
2017-10-30 13:38:48 -0400

    remove unused imports

--- gobii-domain/src/main/java/org/gobiiproject/gobidomain/services/impl/EntityStatsServiceImpl.java
+++ gobii-domain/src/main/java/org/gobiiproject/gobidomain/services/impl/EntityStatsServiceImpl.java
@@ -1,22 +1,14 @@
 package org.gobiiproject.gobidomain.services.impl;
 
 import org.gobiiproject.gobidomain.GobiiDomainException;
-import org.gobiiproject.gobidomain.services.AnalysisService;
 import org.gobiiproject.gobidomain.services.EntityStatsService;
-import org.gobiiproject.gobiidtomapping.auditable.DtoMapAnalysis;
 import org.gobiiproject.gobiidtomapping.entity.noaudit.DtoMapEntityStats;
-import org.gobiiproject.gobiimodel.dto.entity.auditable.AnalysisDTO;
 import org.gobiiproject.gobiimodel.dto.system.EntityStatsDTO;
 import org.gobiiproject.gobiimodel.types.GobiiEntityNameType;
 import org.gobiiproject.gobiimodel.types.GobiiProcessType;
-import org.gobiiproject.gobiimodel.types.GobiiStatusLevel;
-import org.gobiiproject.gobiimodel.types.GobiiValidationStatusType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Phil on 4/21/2016.
