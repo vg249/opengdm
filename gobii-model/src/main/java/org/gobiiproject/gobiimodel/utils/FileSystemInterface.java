f9d2d54a6dd9f3afbe38e60f22539d0bb4c1acf7
Joshua Lamos-Sweeney <jdl232@cornell.edu>
2017-11-28 16:10:56 -0500

    there's enough calls to 'rm' the basic command to justify making -ktf apply to rm(filename) as well.
    Also, added clarifying commentary based on review suggestions.

--- gobii-model/src/main/java/org/gobiiproject/gobiimodel/utils/FileSystemInterface.java
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/utils/FileSystemInterface.java
@@ -18,16 +18,26 @@ public class FileSystemInterface {
 	private static boolean keepAllFiles=false;
 	private static boolean keepInTemp=false;
 	/**
-	 * As unix RM command.
+	 * As unix RM command, except when flags 'keep all files' (-kaf) or 'keep temporary files' (-ktf) are invoked.
+	 * In this case, works as in rmIfExist(described below)
 	 * @param file
 	 */
 	public static void rm(String file){
-		if(keepAllFiles){
+		if(keepAllFiles||keepInTemp){
+			rmIfExist(file);//Deals with 'move to temporary directory' better in the first place
 			return;
 		}
+
 		HelperFunctions.tryExec("rm "+file);
 	}
 
+	/**
+	 * Removes a file only if the file already exists. Note: This method has two global flags that affect its behavior.
+	 * One prevents the removal, and the other moves the file to a \temp directory under the level the file was at.
+	 *
+	 * This behavior is described in the -kaf and -ktf flags.
+	 * @param file File object to be 'removed' as if the RM command was executed on it, but safely in regard to existance of the file.
+	 */
 	public static void rmIfExist(File file){
 		if(file==null)return;
 		if(keepAllFiles){
@@ -47,6 +57,13 @@ public class FileSystemInterface {
 			file.delete();
 		}
 	}
+	/**
+	 * Removes a file only if the file already exists. Note: This method has two global flags that affect its behavior.
+	 * One prevents the removal, and the other moves the file to a \temp directory under the level the file was at.
+	 *
+	 * This behavior is described in the -kaf and -ktf flags.
+	 * @param file File object to be 'removed' as if the RM command was executed on it, but safely in regard to existance of the file.
+	 */
 	public static void rmIfExist(String file){
 		if(file==null)return;
 		File f=new File(file);
