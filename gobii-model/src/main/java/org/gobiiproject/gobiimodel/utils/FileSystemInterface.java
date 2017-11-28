ac850c84c801a37e8775e7c6f9d0f7c3821a07bd
Joshua Lamos-Sweeney <jdl232@cornell.edu>
2017-11-28 15:53:49 -0500

    Added 'keep temporary files' config option. Only occurs in 'rmIfExist', simple RM still kills files dead.

--- gobii-model/src/main/java/org/gobiiproject/gobiimodel/utils/FileSystemInterface.java
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/utils/FileSystemInterface.java
@@ -4,13 +4,19 @@ import org.gobiiproject.gobiimodel.utils.error.ErrorLogger;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 import static org.gobiiproject.gobiimodel.utils.HelperFunctions.tryExec;
 
 public class FileSystemInterface {
 
 	private static boolean keepAllFiles=false;
+	private static boolean keepInTemp=false;
 	/**
 	 * As unix RM command.
 	 * @param file
@@ -26,6 +32,16 @@ public class FileSystemInterface {
 		if(file==null)return;
 		if(keepAllFiles){
 			return;
+		}
+		if(keepInTemp){
+			File tmpDir=new File(file.getParent(),"temp");
+			tmpDir.mkdir(); // returns false if directory exists or is file (if it's a file, whoops).
+			File tempFile=new File(tmpDir,file.getName());
+			try {
+				Files.move(file.toPath(),tempFile.toPath(),(CopyOption)StandardCopyOption.REPLACE_EXISTING);
+			} catch (IOException e) {
+				ErrorLogger.logError("FileSystemInterface",e);
+			}
 		}
 		if(file.exists()){
 			file.delete();
@@ -72,5 +88,13 @@ public class FileSystemInterface {
 	 */
 	public static void keepAllFiles(boolean keep){
 		keepAllFiles=keep;
+	}
+	/**
+	 * Determines whether 'deleted' temporary files will instead be stored in *path*\temp\*filename*
+	 * Only matters if keepAllFiles = false.
+	 * @param keep
+	 */
+	public static void keepTempFiles(boolean keep){
+		keepInTemp=keep;
 	}
 }
