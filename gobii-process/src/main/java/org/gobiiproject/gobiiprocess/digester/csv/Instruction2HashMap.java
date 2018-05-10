3ce3f1347bad399e5048606d6a0c4cbde41a301b
sivasubramanics <siva.subramani975@gmail.com>
2018-05-10 16:18:33 +0530

    Removed SysOuts

--- gobii-process/src/main/java/org/gobiiproject/gobiiprocess/digester/csv/Instruction2HashMap.java
+++ gobii-process/src/main/java/org/gobiiproject/gobiiprocess/digester/csv/Instruction2HashMap.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 
 public class Instruction2HashMap {
 
-    private static String inFile;
     private static String tableName;
     private static String prevLine;
     private static String entityName;
@@ -29,21 +28,17 @@ public class Instruction2HashMap {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 if(line.endsWith("[ {")){
-                    System.out.println(line.toString());
                     tableName = line.split(":")[0].replace("\"","");
-                    System.out.println(tableName);
                     noColumn=1;
                 }
                 if(line.endsWith("} ],") || line.endsWith("}, {")){
                     entityName = prevLine.split(":")[0].replace("\"", "").trim();
                     value = prevLine.split(":")[1].replace("\"", "").trim();
-                    System.out.println(tableName+"\t"+noColumn+"\t"+entityName+"\t"+value);
                 }
                 if(line.endsWith(",") && !line.contains("}")){
                     entityName = line.split(":")[0].replace("\"", "").trim();
                     value = line.split(":")[1].replace("\"", "").trim();
                     map.put(tableName, (InstructionHashmap<Integer, String, String>) instructionMap.put(Integer.hashCode(noColumn), entityName, value));
-                    System.out.println(tableName+"\t"+noColumn+"\t"+entityName+"\t"+value);
                 }
                 if(line.endsWith("}, {")){
                     noColumn++;
@@ -54,9 +49,6 @@ public class Instruction2HashMap {
                 prevLine = line;
             }
             fileReader.close();
-            System.out.println("Contents of file:");
-            System.out.println(stringBuffer.toString());
-
         } catch (Exception e) {
             e.printStackTrace();
         }
