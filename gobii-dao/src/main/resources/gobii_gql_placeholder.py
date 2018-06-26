52d156353cff7a10ee3302750bbd9f12afa3a451
Phil Glaser <pdg66@cornell.edu>
2018-06-26 12:16:00 -0400

    GP1-1749: Add to arg output

--- gobii-dao/src/main/resources/gobii_gql_placeholder.py
+++ gobii-dao/src/main/resources/gobii_gql_placeholder.py
@@ -35,7 +35,7 @@ def main(argv):
     print("Initiated gql placeholder test script with arguments ", sys.argv)
     print("output file is: ", output_file_name)
     print("Target vertex is: ", target_vertex_name)
-    print("subgraph path is: ", sub_graph_path)
+    print("subgraph path is: %s " %sub_graph_path)
     print("max result is: ", max_result)
 
     file_lines = []
