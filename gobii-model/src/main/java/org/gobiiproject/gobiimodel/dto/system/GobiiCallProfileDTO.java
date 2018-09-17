6194a28e2a4238df6e9f12fbdf5476bdd5db60b2
Phil Glaser <pdg66@cornell.edu>
2018-09-17 16:19:00 -0400

    GP1-1890: Add call profile dto

---
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/dto/system/GobiiCallProfileDTO.java
@@ -0,0 +1,53 @@
+package org.gobiiproject.gobiimodel.dto.system;
+
+import org.gobiiproject.gobiimodel.types.RestMethodTypes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GobiiCallProfileDTO {
+
+    public GobiiCallProfileDTO(String methodName, List<RestMethodTypes> supportedVerbs, Integer maxPostPut, Integer maxGet) {
+        this.methodName = methodName;
+        this.supportedVerbs = supportedVerbs;
+        this.maxPostPut = maxPostPut;
+        this.maxGet = maxGet;
+    }
+
+    String methodName;
+    List<RestMethodTypes> supportedVerbs = new ArrayList<>();
+    Integer maxPostPut = 100;
+    Integer maxGet = 500;
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public List<RestMethodTypes> getSupportedVerbs() {
+        return supportedVerbs;
+    }
+
+    public void setSupportedVerbs(List<RestMethodTypes> supportedVerbs) {
+        this.supportedVerbs = supportedVerbs;
+    }
+
+    public Integer getMaxPostPut() {
+        return maxPostPut;
+    }
+
+    public void setMaxPostPut(Integer maxPostPut) {
+        this.maxPostPut = maxPostPut;
+    }
+
+    public Integer getMaxGet() {
+        return maxGet;
+    }
+
+    public void setMaxGet(Integer maxGet) {
+        this.maxGet = maxGet;
+    }
+}
