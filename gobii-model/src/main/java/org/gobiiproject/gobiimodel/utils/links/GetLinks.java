032ec1b32a8f5fdf680c8429b8cfa2bcda931b5e
ssivasurbamani <s.sivasubramani@cgiar.org>
2018-09-19 15:28:12 -0400

    Removed sysout and test main

--- gobii-model/src/main/java/org/gobiiproject/gobiimodel/utils/links/GetLinks.java
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/utils/links/GetLinks.java
@@ -14,28 +14,6 @@ import java.util.Base64;
  */
 public class GetLinks {
 
-    String username;
-    String password;
-    String url;
-    String urlPath;
-
-
-    /**
-     * Just for the develop
-     * @param args
-     * @throws Exception
-     */
-    public static void main(String[] args) throws Exception {
-        ConfigSettings configuration = null ;
-        configuration = new ConfigSettings("D:\\GOBII\\gobii-web.xml");
-//        String urlpath = "http://192.168.80.4:8090/ocs/v1.php/apps/files_sharing/api/v1/shares";
-//        String username = "admin";
-//        String password = "admin";
-//        String path = "path=" + "/data/gobii_bundle/crops/dev/extractor/output/s.sivasubramani/hapmap/whole_dataset/2018_09_17_02_19_02/Dataset.hmp.txt" + "&shareType=3&permissions=1";
-        String path = "/data/gobii_bundle/crops/dev/extractor/output/s.sivasubramani/hapmap/whole_dataset/2018_09_17_02_19_02/Dataset.hmp.txt";
-        System.out.println(getLink(path, configuration));
-    }
-
     /**
      * Simple parser to get the tag value from xml string (Strictly doesnt work for duplicated && nested tags)
      * @param xmlString
@@ -56,8 +34,6 @@ public class GetLinks {
      * @throws Exception
      */
     public static String getLink(String filePath, ConfigSettings config) throws Exception {
-//        String urlPath, String username, String password, String path
-
         String username = config.getGlobalServer(GobiiServerType.OWN_CLOUD).getUserName();
         String password = config.getGlobalServer(GobiiServerType.OWN_CLOUD).getPassword();
         String host = config.getGlobalServer(GobiiServerType.OWN_CLOUD).getHost();
@@ -86,10 +62,6 @@ public class GetLinks {
             resultBuf.append((char) c);
         }
         input.close();
-        System.out.println(path);
-        System.out.println(urlPath);
-        System.out.println(userpass);
-        System.out.println(resultBuf.toString());
         if(getTAG(resultBuf.toString(),"status").equals("ok")){
             liveLink = getTAG(resultBuf.toString(),"url") + "/download";
         }
@@ -103,7 +75,6 @@ public class GetLinks {
         String host = config.getGlobalServer(GobiiServerType.OWN_CLOUD).getHost();
         String port = config.getGlobalServer(GobiiServerType.OWN_CLOUD).getPort().toString();
         String liveURL = "http://" + host + (port.equals("") ? "" : (":" + port)) + "/" + "/apps/files/?dir=" + path;
-//        String liveURL = "http://" + "192.168.80.4:8090" + "/apps/files/?dir=" + "/gobiidev/gobii_bundle/crops/dev/files/2018-05-30_16-35-51-683";
         return liveURL;
     }
 }
