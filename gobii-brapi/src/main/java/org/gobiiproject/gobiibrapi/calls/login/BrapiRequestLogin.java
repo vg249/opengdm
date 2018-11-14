ade62205f556e35ea99c407cebdbd6a93f4f2327
Phil Glaser <pdg66@cornell.edu>
2018-11-14 09:41:22 -0500

    GP1-2055: Begin adding model

--- gobii-brapi/src/main/java/org/gobiiproject/gobiibrapi/calls/login/BrapiRequestLogin.java
+++ gobii-brapi/src/main/java/org/gobiiproject/gobiibrapi/calls/login/BrapiRequestLogin.java
@@ -1,120 +1,53 @@
-package org.gobiiproject.gobiibrapi.calls.studies.search;
+package org.gobiiproject.gobiibrapi.calls.login;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
 /**
  * Created by Phil on 12/15/2016.
  */
-public class BrapiRequestStudiesSearch {
+public class BrapiRequestLogin {
 
+    private String clientId;
+    private String grantType;
+    private String password;
+    private String userName;
 
-    private String studyType;
-    private List<String> studyNames;
-    private List<String> studyLocations;
-    private List<String> programNames;
-    private List<String> germplasmDbIds;
-    private List<String> trialDbIds;
-    private List<String> observationVariableDbIds;
-    private boolean active;
-    private String sortBy;
-    private String sortOrder;
-    private Integer pageSize;
-    private Integer page;
-
-    public String getStudyType() {
-        return studyType;
+    @JsonProperty("client_id")
+    public String getClientId() {
+        return clientId;
     }
 
-    public void setStudyType(String studyType) {
-        this.studyType = studyType;
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
     }
 
-    public List<String> getStudyNames() {
-        return studyNames;
+    @JsonProperty("grant_type")
+    public String getGrantType() {
+        return grantType;
     }
 
-    public void setStudyNames(List<String> studyNames) {
-        this.studyNames = studyNames;
-    }
-
-    public List<String> getStudyLocations() {
-        return studyLocations;
-    }
-
-    public void setStudyLocations(List<String> studyLocations) {
-        this.studyLocations = studyLocations;
-    }
-
-    public List<String> getProgramNames() {
-        return programNames;
-    }
-
-    public void setProgramNames(List<String> programNames) {
-        this.programNames = programNames;
-    }
-
-    public List<String> getGermplasmDbIds() {
-        return germplasmDbIds;
-    }
-
-    public void setGermplasmDbIds(List<String> germplasmDbIds) {
-        this.germplasmDbIds = germplasmDbIds;
-    }
-
-    public List<String> getObservationVariableDbIds() {
-        return observationVariableDbIds;
-    }
-
-    public void setObservationVariableDbIds(List<String> observationVariableDbIds) {
-        this.observationVariableDbIds = observationVariableDbIds;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public String getSortBy() {
-        return sortBy;
-    }
-
-    public void setSortBy(String sortBy) {
-        this.sortBy = sortBy;
-    }
-
-    public String getSortOrder() {
-        return sortOrder;
-    }
-
-    public void setSortOrder(String sortOrder) {
-        this.sortOrder = sortOrder;
+    public void setGrantType(String grantType) {
+        this.grantType = grantType;
     }
 
 
-    public List<String> getTrialDbIds() {
-        return trialDbIds;
+    @JsonProperty("password")
+    public String getPassword() {
+        return password;
     }
 
-    public void setTrialDbIds(List<String> trialDbIds) {
-        this.trialDbIds = trialDbIds;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public Integer getPageSize() {
-        return pageSize;
+    @JsonProperty("username")
+    public String getUserName() {
+        return userName;
     }
 
-    public void setPageSize(Integer pageSize) {
-        this.pageSize = pageSize;
-    }
-
-    public Integer getPage() {
-        return page;
-    }
-
-    public void setPage(Integer page) {
-        this.page = page;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
