0885c46818213192af130b76ee676ffb9fca4f32
araquel <a.raquel@irri.org>
2018-12-21 17:45:12 +0800

    GSD-443 Add Ok button to close dialog window for list size pop-up

--- gobii-web/src/main/webapp/js/gobii_modules/views/sample-marker-box.component.ts
+++ gobii-web/src/main/webapp/js/gobii_modules/views/sample-marker-box.component.ts
@@ -102,6 +102,10 @@ import {ViewIdGeneratorService} from "../services/core/view-id-generator-service
                           modal="modal" [dismissableMask]="true" width="300" height="200" [responsive]="true">
                     <p>You attempted to paste more than {{maxListItems}} {{maxExceededTypeLabel}} items; Please reduce
                         the size of the list</p>
+
+                    <p-footer>
+                        <button type="button" pButton (click)="displayMaxItemsExceeded=false" label="OK"></button>
+                    </p-footer>
                 </p-dialog>
            
         </div>`
