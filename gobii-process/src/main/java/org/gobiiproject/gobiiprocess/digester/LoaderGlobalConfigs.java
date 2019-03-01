f9fdaa14daa3e6b49e62799140d0dea9282a2aa1
Joshua Lamos-Sweeney <jdl232@cornell.edu>
2019-03-01 15:46:44 -0500

    Simple disabler for non-matrix validation routines and flag to enable for testing

--- gobii-process/src/main/java/org/gobiiproject/gobiiprocess/digester/LoaderGlobalConfigs.java
+++ gobii-process/src/main/java/org/gobiiproject/gobiiprocess/digester/LoaderGlobalConfigs.java
@@ -7,7 +7,7 @@ public class LoaderGlobalConfigs extends ProcessGlobalConfigs{
     private LoaderGlobalConfigs(){}
     private static boolean singleThreadFileRead=false;
     private static boolean deleteIntermediateFiles=false;
-
+    private static boolean enableValidation=false;//TODO- make true to enable validation by default
 
     /**
      * Adds options to an Options object which will be read in 'setFromFlags'.
@@ -16,15 +16,20 @@ public class LoaderGlobalConfigs extends ProcessGlobalConfigs{
         ProcessGlobalConfigs.addOptions(o);
         o.addOption("str", "singleThreadRead", false, "Use a single thread for file reading");
         o.addOption("dif","deleteIntermediateFiles", false, "Delete intermediate files (and save some space)");
+        o.addOption("ev","enableValidation",true,"Enable data validation [true/[false]]");
     }
     public static void setFromFlags(CommandLine cli){
         ProcessGlobalConfigs.setFromFlags(cli);
         if(cli.hasOption("singleThreadRead")) singleThreadFileRead=true;
         if(cli.hasOption("deleteIntermediateFiles")) deleteIntermediateFiles=true;
+        if(cli.hasOption("enableValidation")){
+            enableValidation=Boolean.parseBoolean(cli.getOptionValue("enableValidation"));//True on 'true' 'TRUE' 'tRuE', false on anything else.
+        }
     }
 
     public static boolean getSingleThreadFileRead(){
         return singleThreadFileRead;
     }
     public static boolean getDeleteIntermediateFiles() { return deleteIntermediateFiles; }
+    public static boolean getValidation() { return enableValidation; }
 }
