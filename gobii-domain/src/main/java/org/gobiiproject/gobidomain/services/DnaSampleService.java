0fc7853a0488d3efd25c52af1a5eb4966abf676f
Yanii <v.calaminos@irri.org>
2019-05-02 16:03:49 +0200

    GP1-2289 Prepare classes for Dna Sample services

---
+++ gobii-domain/src/main/java/org/gobiiproject/gobidomain/services/DnaSampleService.java
@@ -0,0 +1,14 @@
+package org.gobiiproject.gobidomain.services;
+
+import org.gobiiproject.gobidomain.GobiiDomainException;
+
+import java.util.List;
+
+/**
+ * Created by VCalaminos on 5/2/2019.
+ */
+public interface DnaSampleService<T> {
+
+    List<T> createSamples(List<T> sampleListDTO, Integer projectId) throws GobiiDomainException;
+
+}
