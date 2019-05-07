c643bad7f7b1a73ecbbeb3b624b1a5e14998a8b8
Vishnu <vg249@cornell.edu>
2019-05-07 13:42:23 -0400

    Renamed ControllerExceptionHandler from ErrorController to GlobalControllerExceptionHandler

--- gobii-web/src/main/java/org/gobiiproject/gobiiweb/controllers/GlobalControllerExceptionHandler.java
+++ gobii-web/src/main/java/org/gobiiproject/gobiiweb/controllers/GlobalControllerExceptionHandler.java
@@ -2,19 +2,13 @@ package org.gobiiproject.gobiiweb.controllers;
 
 import org.gobiiproject.gobiiapimodel.payload.sampletracking.ErrorPayload;
 import org.gobiiproject.gobiimodel.config.GobiiException;
-import org.gobiiproject.gobiimodel.types.GobiiValidationStatusType;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import java.sql.SQLIntegrityConstraintViolationException;
 
 @ControllerAdvice
-public class ErrorController {
+public class GlobalControllerExceptionHandler {
 
     @ExceptionHandler(GobiiException.class)
     public ResponseEntity GobiiExceptionHandler(GobiiException gEx) {
