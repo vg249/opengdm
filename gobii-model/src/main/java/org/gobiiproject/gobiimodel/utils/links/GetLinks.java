6799dce920885fe236ba20dc5356729d0f8c78c1
Joshua Lamos-Sweeney <jdl232@cornell.edu>
2019-06-17 10:27:30 -0400

    Added more error checking to GetLinks

--- gobii-model/src/main/java/org/gobiiproject/gobiimodel/utils/links/GetLinks.java
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/utils/links/GetLinks.java
@@ -7,6 +7,8 @@ import org.gobiiproject.gobiimodel.utils.error.ErrorLogger;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Base64;
@@ -37,7 +39,7 @@ public class GetLinks {
      */
     public static String getLink(String filePath, ConfigSettings config) throws Exception {
 
-        if(filePath.contains("ïnprogress")){
+        if (filePath.contains("ïnprogress")) {
             filePath.replace("inprogress", "done");
         }
 
@@ -48,45 +50,54 @@ public class GetLinks {
         String port = ocConf.getPort().toString();
         String contextPath = ocConf.getContextPath();
         String alternateContextPath = ocConf.getErrorContextPath();
-        String urlPath = "http://" + host + (port.equals("") ? "" : ":"+port) + "/ocs/v1.php/apps/files_sharing/api/v1/shares";
+        String urlPath = "http://" + host + (port.equals("") ? "" : ":" + port) + "/ocs/v1.php/apps/files_sharing/api/v1/shares";
         String relativeContext = contextPath;
         String path;
-        if(filePath.contains("/logs/")){
-            relativeContext=alternateContextPath;
+        if (filePath.contains("/logs/")) {
+            relativeContext = alternateContextPath;
             path = "path=" + filePath.replace("/data/gobii_bundle/logs/", "/" + relativeContext) + "&shareType=3&permissions=1";
 
-        }
-        else {
+        } else {
             path = "path=" + filePath.replace("/data/gobii_bundle/crops/", "/" + relativeContext) + "&shareType=3&permissions=1";
         }
         String liveLink;
-        URL url = new URL(urlPath);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        String userpass = username + ":" + password;
-        String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userpass.getBytes()));
-        connection.setRequestProperty ("Authorization", basicAuth);
-        connection.setRequestMethod("POST");
-        connection.setInstanceFollowRedirects(true);
-        connection.setRequestProperty("Content-length",
-        String.valueOf(path.length()));
-        connection.setDoOutput(true);
-        DataOutputStream output = new DataOutputStream(connection.getOutputStream());
-        output.writeBytes(path);
-        output.close();
-        DataInputStream input = new DataInputStream(connection.getInputStream());
-        int c;
-        StringBuilder resultBuf = new StringBuilder();
-        while ((c = input.read()) != -1) {
-            resultBuf.append((char) c);
+        try {
+            URL url = new URL(urlPath);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            String userpass = username + ":" + password;
+            String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userpass.getBytes()));
+            connection.setRequestProperty("Authorization", basicAuth);
+            connection.setRequestMethod("POST");
+            connection.setInstanceFollowRedirects(true);
+            connection.setRequestProperty("Content-length",
+                    String.valueOf(path.length()));
+            connection.setDoOutput(true);
+            DataOutputStream output = new DataOutputStream(connection.getOutputStream());
+            output.writeBytes(path);
+            output.close();
+            DataInputStream input = new DataInputStream(connection.getInputStream());
+            int c;
+            StringBuilder resultBuf = new StringBuilder();
+            while ((c = input.read()) != -1) {
+                resultBuf.append((char) c);
+            }
+            input.close();
+            if (getTAG(resultBuf.toString(), "status").equals("ok")) {
+                liveLink = getTAG(resultBuf.toString(), "url") + "/download";
+            } else {
+                liveLink = "";
+            ErrorLogger.logWarning("OWNCLOUD", "API request failed due to improper configurations");
+            }
         }
-        input.close();
-        if(getTAG(resultBuf.toString(),"status").equals("ok")){
-            liveLink = getTAG(resultBuf.toString(),"url") + "/download";
+        catch(ConnectException e){
+            liveLink="";
+            ErrorLogger.logWarning("OWNCLOUD", "API request failed due to improper configurations",e);
         }
-        else{
-            liveLink = "";
-//            ErrorLogger.logError("OWNCLOUD", "API request failed due to improper configurations");
+        catch(IOException e){
+            liveLink="";
+            ErrorLogger.logWarning("OWNCLOUD", "API request failed due to improper configurations",e);
         }
+
         return liveLink.replace("http://","URL:");
     }
 
