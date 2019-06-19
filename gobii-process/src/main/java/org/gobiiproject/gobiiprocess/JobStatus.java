84df6eec981c0e37274a473596f72afd27572588
ljc237 <ljc237@cornell.edu>
2019-06-19 13:24:27 -0400

    Simple code structure reformatting for easier reading

--- gobii-process/src/main/java/org/gobiiproject/gobiiprocess/JobStatus.java
+++ gobii-process/src/main/java/org/gobiiproject/gobiiprocess/JobStatus.java
@@ -33,8 +33,8 @@ public class JobStatus {
      * @param jobName the name of the instruction file, minus the .json suffix
      */
     GobiiUriFactory uriFactory;
-    String jobName;
-	JobDTO lastStatus;
+	JobDTO job;
+
 	/**List of valid statuses. Update as appropriate. Stops 'random argument' assignment, even though Progress Status
 	 * has been stringly typed.
 	 */
@@ -54,65 +54,70 @@ public class JobStatus {
 	));
 
     public JobStatus(ConfigSettings config, String cropName, String jobName) throws Exception {
-		this.jobName=jobName;
+
+		this.job = new JobDTO();
+		job.setJobName(jobName);
 		// set up authentication and so forth
 		// you'll need to get the current from the instruction file
 		GobiiClientContext context = GobiiClientContext.getInstance(config, cropName, GobiiAutoLoginType.USER_RUN_AS);
 		uriFactory = context.getUriFactory();
     }
 
-    public void set(JobProgressStatusType status,String message){
-    	if(status==null || !acceptedStatuses.contains(status)){
+    public void set(JobProgressStatusType status, String message){
+
+    	if(!acceptedStatuses.contains(status)){
     		ErrorLogger.logError(
     				"JobStatus",
 					String.format("Invalid status passed to set: %s\nMessage: %s", status.getCvName(), message),
 					new Exception());//passing a new exception throws a stack trace in there
 		}
-            try{
-                RestUri restUri=uriFactory
-                        .resourceByUriIdParam(RestResourceId.GOBII_JOB);
-                restUri.setParamValue("id", jobName);
-            GobiiEnvelopeRestResource<JobDTO,JobDTO> gobiiEnvelopeRestResource = new GobiiEnvelopeRestResource<>(restUri);
-            PayloadEnvelope<JobDTO> resultEnvelope = gobiiEnvelopeRestResource
-                    .get(JobDTO.class);
 
-				JobDTO dataSetResponse;
-            if (!resultEnvelope.getHeader().getStatus().isSucceeded()) {
-                System.out.println();
-                logError("Digester", "Job table response errors");
-                for (HeaderStatusMessage currentStatusMesage : resultEnvelope.getHeader().getStatus().getStatusMessages()) {
-                    logError("HeaderError", currentStatusMesage.getMessage());
-                }
-                return;
-			}
-				List<JobDTO> responses = resultEnvelope.getPayload().getData();
-				if(responses.size()==0){
-					logError("JobStatus","No Job record returned for job " + jobName);
-					return;
+		try {
+			RestUri restUri = uriFactory.resourceByUriIdParam(RestResourceId.GOBII_JOB);
+			restUri.setParamValue("id", job.getJobName());
+
+			GobiiEnvelopeRestResource<JobDTO,JobDTO> gobiiEnvelopeRestResource = new GobiiEnvelopeRestResource<>(restUri);
+			PayloadEnvelope<JobDTO> resultEnvelope = gobiiEnvelopeRestResource.get(JobDTO.class);
+
+			if (!resultEnvelope.getHeader().getStatus().isSucceeded()) {
+				System.out.println();
+				logError("Digester", "Job table response errors");
+
+				for (HeaderStatusMessage currentStatusMesage : resultEnvelope.getHeader().getStatus().getStatusMessages()) {
+					logError("HeaderError", currentStatusMesage.getMessage());
 				}
-				dataSetResponse = responses.get(0);
-
-
-				dataSetResponse.setMessage(message);
-				dataSetResponse.setStatus(status.getCvName());
-
-				resultEnvelope = gobiiEnvelopeRestResource
-						.put(JobDTO.class, new PayloadEnvelope<>(dataSetResponse, GobiiProcessType.UPDATE));
-
-				//Set 'lastStatus' to the current status
-				lastStatus = dataSetResponse;
-				// if you didn't succeed, do not pass go, but do log errors to your log file
-				if (!resultEnvelope.getHeader().getStatus().isSucceeded()) {
-					logError("Digester", "Data set response response errors");
-					for (HeaderStatusMessage currentStatusMesage : resultEnvelope.getHeader().getStatus().getStatusMessages()) {
-						logError("HeaderError", currentStatusMesage.getMessage());
-					}
-					return;
-				}
-			} catch (Exception e) {
-				logError("Digester", "Exception while referencing Job table in Postgresql", e);
 				return;
 			}
+
+			List<JobDTO> responses = resultEnvelope.getPayload().getData();
+			if(responses.size() == 0) {
+				logError("JobStatus", String.format("No Job record returned for job %s", job.getJobName()));
+				return;
+			}
+
+			JobDTO dataSetResponse = responses.get(0);
+
+			dataSetResponse.setMessage(message);
+			dataSetResponse.setStatus(status.getCvName());
+
+			resultEnvelope = gobiiEnvelopeRestResource
+					.put(JobDTO.class, new PayloadEnvelope<>(dataSetResponse, GobiiProcessType.UPDATE));
+
+			//Set 'job' to the current status
+			job = dataSetResponse;
+
+			// if you didn't succeed, do not pass go, but do log errors to your log file
+			if (!resultEnvelope.getHeader().getStatus().isSucceeded()) {
+				logError("Digester", "Data set response response errors");
+				for (HeaderStatusMessage currentStatusMesage : resultEnvelope.getHeader().getStatus().getStatusMessages()) {
+					logError("HeaderError", currentStatusMesage.getMessage());
+				}
+				return;
+			}
+		} catch (Exception e) {
+			logError("Digester", "Exception while referencing Job table in Postgresql", e);
+			return;
+		}
 	}
 
 	/**
@@ -120,11 +125,12 @@ public class JobStatus {
      *
      */
 	public void setError(String message){
+
         String errorMessage="";
-        if(lastStatus!=null){
-            errorMessage="Status: " + lastStatus.getStatus()+" - " + lastStatus.getMessage() + " | \n";
+        if(job !=null){
+            errorMessage = String.format("Status: %s - %s | \n", job.getStatus(), job.getMessage());
         }
-        errorMessage += message + " : " + ErrorLogger.getFirstErrorReason();
+        errorMessage += String.format("%s : %s", message, ErrorLogger.getFirstErrorReason());
         set(JobProgressStatusType.CV_PROGRESSSTATUS_FAILED,errorMessage);
     }
 }
