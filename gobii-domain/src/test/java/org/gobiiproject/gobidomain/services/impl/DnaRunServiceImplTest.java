dc11fd779c8fba21fc57e3b8129b5ca8051fcddc
Yanii <v.calaminos@irri.org>
2019-06-28 10:27:11 -0400

    GDM-230 Added test cases for getting callsets with filters

--- gobii-domain/src/test/java/org/gobiiproject/gobidomain/services/impl/DnaRunServiceImplTest.java
+++ gobii-domain/src/test/java/org/gobiiproject/gobidomain/services/impl/DnaRunServiceImplTest.java
@@ -14,7 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.lang.reflect.Field;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static junit.framework.Assert.assertNotNull;
@@ -69,6 +73,105 @@ public class DnaRunServiceImplTest {
 
         assertTrue(dnaRunDTOList.size() == pageSize);
         assertNotNull(dnaRunDTOList.get(0).getCallSetDbId());
+    }
+
+    @Test
+    public void getDnaRunListWithFilterByVariantDbSetId() {
+
+        // get all dnaruns to have an existing callsetname we can use as an example
+        List<DnaRunDTO> dnaRunDTOList;
+        dnaRunDTOList = dnaRunService.getDnaRuns(null, null, null);
+
+        if (dnaRunDTOList.size() <= 0) {
+            throw new AssumptionViolatedException("There are no available dna runs in the database");
+        }
+
+        // check for dnarun that has existing dataset Ids
+        Integer variantSetDbId = null;
+
+        for (DnaRunDTO currentDnaRunDTO : dnaRunDTOList) {
+
+            if (currentDnaRunDTO.getVariantSetIds().size() > 0) {
+                variantSetDbId = currentDnaRunDTO.getVariantSetIds().get(0);
+                break;
+            }
+
+        }
+
+        if (variantSetDbId == null) {
+            throw new AssumptionViolatedException("There are no available dna runs with matching datasetIds in the database");
+        }
+
+
+        DnaRunDTO dnaRunDTOFilter = new DnaRunDTO();
+        dnaRunDTOFilter.getVariantSetIds().add(variantSetDbId);
+
+        List<DnaRunDTO> filteredDnaRunDTOList = dnaRunService.getDnaRuns(null, null, dnaRunDTOFilter);
+
+        assertTrue(filteredDnaRunDTOList.size() > 0);
+        assertNotNull(filteredDnaRunDTOList.get(0).getVariantSetIds());
+        assertTrue(filteredDnaRunDTOList.get(0).getVariantSetIds().contains(variantSetDbId));
+
+    }
+
+    @Test
+    public void getDnaRunListWithFilterByCallSetName() throws Exception {
+        getFilteredListDnaRun("callSetName");
+    }
+
+    @Test
+    public void getDnaRunListWithFilterBySampleDbId() throws Exception {
+        getFilteredListDnaRun("sampleDbId");
+    }
+
+    @Test
+    public void getDnaRunListWithFilterByGermplasmDbId() throws Exception {
+        getFilteredListDnaRun("germplasmDbId");
+    }
+
+    @Test
+    public void getDnaRunListWithFilterByStudyDbId() throws Exception {
+        getFilteredListDnaRun("studyDbId");
+    }
+
+    private void getFilteredListDnaRun(String filterKey) throws Exception {
+
+        // get all dnaruns to have an existing callsetname we can use as an example
+        List<DnaRunDTO> dnaRunDTOList;
+        dnaRunDTOList = dnaRunService.getDnaRuns(null, null, null);
+
+        if (dnaRunDTOList.size() <= 0) {
+            throw new AssumptionViolatedException("There are no available dna runs in the database");
+        }
+
+        Field field = DnaRunDTO.class.getDeclaredField(filterKey);
+        field.setAccessible(true);
+
+        assertNotNull(field.get(dnaRunDTOList.get(0)));
+
+        String filterValue = field.get(dnaRunDTOList.get(0)).toString();
+
+        DnaRunDTO dnaRunDTOFilter = new DnaRunDTO();
+        field.set(dnaRunDTOFilter, processTypes(filterValue, field.getType()));
+
+        List<DnaRunDTO> filteredDnaRunDTOList = dnaRunService.getDnaRuns(null, null, dnaRunDTOFilter);
+
+        assertTrue(filteredDnaRunDTOList.size() > 0);
+        assertNotNull(field.get(filteredDnaRunDTOList.get(0)).toString());
+        assertTrue(field.get(filteredDnaRunDTOList.get(0)).toString().equals(filterValue));
+
+    }
+
+    private static <E> E processTypes(Object value, Class<E> type) throws ParseException {
+
+        if (type.equals(Integer.class)) {
+            return type.cast(Integer.parseInt(value.toString()));
+        } else if (type.equals(Date.class)) {
+            SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+            Date date = formatter.parse(value.toString());
+            return type.cast(date);
+        }
+        return type.cast(value);
     }
 
     @Test
