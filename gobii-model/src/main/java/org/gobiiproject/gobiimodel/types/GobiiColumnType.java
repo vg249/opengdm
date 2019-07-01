7f0784d8285ec8b68e8e05fa637951f1770d6a7b
ljc237 <ljc237@cornell.edu>
2019-07-01 12:55:33 -0400

    Removed VCF* GobiiColumnType

--- gobii-model/src/main/java/org/gobiiproject/gobiimodel/types/GobiiColumnType.java
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/types/GobiiColumnType.java
@@ -7,12 +7,6 @@ public enum GobiiColumnType {
     CSV_COLUMN,
     CSV_ROW,
     CSV_BOTH,
-    VCF_SAMPLE,
-    VCF_MARKER,
-    VCF_VARIANT,
-    VCF_METADATA,
-    VCF_INFO,
     CONSTANT,
-    AUTOINCREMENT,
-    VCF_MARKER_POS
+    AUTOINCREMENT
 }
