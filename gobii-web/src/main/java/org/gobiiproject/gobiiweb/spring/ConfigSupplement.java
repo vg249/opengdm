1d7a0148b2990224731af6b03ff6cad4f1761b8e
root <vg249@cornell.edu>
2019-07-24 15:04:49 -0400

    revert config settings change

--- gobii-web/src/main/java/org/gobiiproject/gobiiweb/spring/ConfigSupplement.java
+++ gobii-web/src/main/java/org/gobiiproject/gobiiweb/spring/ConfigSupplement.java
@@ -32,13 +32,6 @@ public class ConfigSupplement {
 
     private static String CONFIG_FILE_LOCATION_PROP = "cfgFqpn";
 
-    private ConfigSettings configSettings;
-
-    public ConfigSupplement() {
-        String configFileLocation = System.getProperty(CONFIG_FILE_LOCATION_PROP);
-        this.configSettings = new ConfigSettings(configFileLocation);
-    }
-
     @Bean(name="dataSourceMulti")
     public DataSourceSelector dataSourceMulti() throws Exception {
 
@@ -92,9 +85,11 @@ public class ConfigSupplement {
 
         returnVal.setCurrentRequest(this.currentRequest);
 
+        ConfigSettings configSettings = new ConfigSettings();
+
         Map<String, Object> hdf5ProcessPathsByCrop = new HashMap<>();
 
-        for (GobiiCropConfig currentGobiiCropConfig : this.configSettings.getActiveCropConfigs()) {
+        for (GobiiCropConfig currentGobiiCropConfig : configSettings.getActiveCropConfigs()) {
            Map<String, String> hdf5ProcessPaths = new HashMap<>();
 
            String gobiiCropType = currentGobiiCropConfig.getGobiiCropType();
