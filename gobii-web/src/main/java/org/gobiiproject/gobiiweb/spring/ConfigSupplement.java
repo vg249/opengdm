905d20880489ebee32df88151ca67f118486b1f1
root <vg249@cornell.edu>
2019-07-25 16:18:34 -0400

    Revert "Config settings change"
    This reverts commit ae5aaaa9b5df679db9fa6770b97ca63e77876518.

--- gobii-web/src/main/java/org/gobiiproject/gobiiweb/spring/ConfigSupplement.java
+++ gobii-web/src/main/java/org/gobiiproject/gobiiweb/spring/ConfigSupplement.java
@@ -46,11 +46,10 @@ public class ConfigSupplement {
 
         returnVal.setCurrentRequest(currentRequest);
 
-        ConfigSettings configSettings = new ConfigSettings();
 
         Map<Object,Object> targetDataSources = new HashMap<>();
 
-        for (GobiiCropConfig currentGobiiCropConfig : configSettings.getActiveCropConfigs()) {
+        for (GobiiCropConfig currentGobiiCropConfig : this.configSettings.getActiveCropConfigs()) {
 
             //Sets Postgres settings
             ServerConfig currentPostGresConfig = currentGobiiCropConfig.getServer(ServerType.GOBII_PGSQL);
