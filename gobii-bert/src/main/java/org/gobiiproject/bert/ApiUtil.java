7b84c8e0fb74e5e0237028edcecf5619f6a0f424
Luke Cook <invocatils@gmail.com>
2019-08-23 13:09:15 -0400

    Creating actions for bert interacting with api: create for each project, platform, experiment, and protocol. Api request bodies have been kept to basics until futher research can be done

--- gobii-bert/src/main/java/org/gobiiproject/bert/ApiUtil.java
+++ gobii-bert/src/main/java/org/gobiiproject/bert/ApiUtil.java
@@ -6,10 +6,14 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 public class ApiUtil {
+
+	private static final String PAYLOAD_ENVELOPE_JSON_TEMPLATE = "{\"payload\": {\"data\": %s}}";
 
 	private HttpURLConnection connect(String method, String urlString, Map<String, String> params) throws IOException {
 
@@ -29,7 +33,7 @@ public class ApiUtil {
 	private static Map<String, Object> makeParamMap(Object ... params) {
 
 		if (! (params.length % 2 == 0)) {
-			throw new RuntimeException("Params must be an even number");
+			throw new RuntimeException("An even number of params must be passed");
 		}
 
 		Map<String, Object> paramMap = new HashMap<>();
@@ -51,4 +55,12 @@ public class ApiUtil {
 		return new RestTemplate().postForEntity(url, object, as, makeParamMap(params));
 	}
 
+	public static String makeEnvelopeJson(String template, Object ... params) {
+		return String.format(PAYLOAD_ENVELOPE_JSON_TEMPLATE,
+				String.format(template, params));
+	}
+
+	public static <T> T result(ResponseEntity<JsonNode> response, Class<T> as) {
+		return new ObjectMapper().convertValue(response.getBody().get("payload").get("data"), as);
+	}
 }
--- gobii-bert/src/main/java/org/gobiiproject/bert/components/Api.java
+++ gobii-bert/src/main/java/org/gobiiproject/bert/components/Api.java
@@ -1,28 +1,99 @@
 package org.gobiiproject.bert.components;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import ernie.core.Action;
-import org.gobiiproject.bert.ApiUtil;
+import org.gobiiproject.gobiimodel.dto.entity.auditable.ExperimentDTO;
+import org.gobiiproject.gobiimodel.dto.entity.auditable.PlatformDTO;
 import org.gobiiproject.gobiimodel.dto.entity.auditable.ProjectDTO;
+import org.gobiiproject.gobiimodel.dto.entity.auditable.ProtocolDTO;
+import org.springframework.http.ResponseEntity;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
+import static org.gobiiproject.bert.ApiUtil.*;
 
 public class Api {
 
+	private static final String URL_PING = "%s/ping";
+	private static final String URL_CONTACT_SEARCH = "%s/contact-search";
 	private static final String URL_PROJECT = "%s/projects";
+	private static final String URL_EXPERIMENTS = "%s/experiments";
+	private static final String URL_PROTOCOLS = "%s/protocols";
+	private static final String URL_PLATFORMS = "%s/platforms";
+
+	private static final String PROJECT_JSON_TEMPLATE = "{\"projectName\": %s}";
+	private static final String PROTOCOL_JSON_TEMPLATE = "{\"protocolName\": %s}";
+	private static final String PLATFORM_JSON_TEMPLATE = "{\"platformName\": %s}";
+	private static final String EXPERIMENT_JSON_TEMPLATE = "{\"projectId\": %s, \"experimentName\": %s}";
 
 	private String url;
 
+	private String url(String endpoint) {
+		return String.format(endpoint, url);
+	}
+
 	@Action("host")
 	public void host(String url) {
+
+		post(url(URL_PING), "", JsonNode.class);
+
 		this.url = url;
 	}
 
-	@Action("project")
-	public void createProject(String name) {
+	@Action("principalInvestigator")
+	public Integer principalInvestigator(String name) throws Exception {
 
+		String[] tokens = name.split(" ");
+
+		ResponseEntity<JsonNode> response;
+
+		if (tokens.length == 1) {
+			response = get(url(URL_CONTACT_SEARCH), JsonNode.class,
+					"firstName", tokens[0]);
+		} else if (tokens.length == 2) {
+			response = get(url(URL_CONTACT_SEARCH), JsonNode.class,
+					"firstName", tokens[0], "lastName", tokens[1]);
+		} else {
+			throw new Exception("Principal Investigator should be provide a first name, or a first name and a last name");
+		}
+
+		return response.getBody().get("payload").get("data").get(0).get("contactId").asInt();
 	}
+
+	@Action("project")
+	public ProjectDTO createProject(Integer principalInvestigatorId, String name) {
+
+		String json = makeEnvelopeJson(PROJECT_JSON_TEMPLATE, name);
+
+		ResponseEntity<JsonNode> response = post(url(URL_PROJECT), json, JsonNode.class);
+
+		return result(response, ProjectDTO.class);
+	}
+
+	@Action("protocol")
+	public ProtocolDTO createProtocol(String name) {
+		String json = makeEnvelopeJson(PROTOCOL_JSON_TEMPLATE, name);
+
+		ResponseEntity<JsonNode> response = post(url(URL_PROTOCOLS), json, JsonNode.class);
+
+		return result(response, ProtocolDTO.class);
+	}
+
+	@Action("platform")
+	public PlatformDTO createPlatform(String name) {
+		String json = makeEnvelopeJson(PLATFORM_JSON_TEMPLATE, name);
+
+		ResponseEntity<JsonNode> response = post(url(URL_PLATFORMS), json, JsonNode.class);
+
+		return result(response, PlatformDTO.class);
+	}
+
+	@Action("experiment")
+	public ExperimentDTO createExperiment(ProjectDTO project, String name) {
+
+		String json = makeEnvelopeJson(EXPERIMENT_JSON_TEMPLATE, project.getId(), name);
+
+		ResponseEntity<JsonNode> response = post(url(URL_EXPERIMENTS), json, JsonNode.class);
+
+		return result(response, ExperimentDTO.class);
+	}
+
 }
