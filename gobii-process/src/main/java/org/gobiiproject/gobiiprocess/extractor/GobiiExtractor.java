5fe7b082299b7d43f3f65d5524b4f6c5fb8ede88
Joshua Lamos-Sweeney <jdl232@cornell.edu>
2019-08-29 16:57:38 -0400

    Best we can do is hope there were no errors, because emails are sent per job record in the file, not once the file is complete

--- gobii-process/src/main/java/org/gobiiproject/gobiiprocess/extractor/GobiiExtractor.java
+++ gobii-process/src/main/java/org/gobiiproject/gobiiprocess/extractor/GobiiExtractor.java
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import org.apache.commons.cli.*;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.http.HttpStatus;
 import org.gobiiproject.gobiiapimodel.payload.HeaderStatusMessage;
 import org.gobiiproject.gobiiapimodel.payload.PayloadEnvelope;
@@ -476,7 +477,9 @@ public class GobiiExtractor {
 			            pm.addCriteria("Mapset List", String.join("<BR>", inst.getMapsetIds().toString())); //This should never happen
 		            }
 
-		            pm.addPath("Instruction File", new File(instructionFile).getAbsolutePath(), true, configuration, false);
+		            //Note - link to place where it *will* be if there are no errors. Sadly, we won't know if it's right until we've sent all the emails already
+		            String finalIFPath= configuration.getProcessingPath(firstCrop, GobiiFileProcessDir.EXTRACTOR_DONE) + FilenameUtils.getName(instructionFile);
+		            pm.addPath("Instruction File", finalIFPath , true, configuration, false);
 		            pm.addFolderPath("Output Directory", extractDir, configuration);
 		            pm.addPath("Error Log", logFile, true, configuration, false);
 		            pm.addPath("Summary File", new File(projectFile).getAbsolutePath(), configuration, false);
