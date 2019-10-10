4483500ee1a0fd74fd83510bbeaa417afccfccf0
Vishnu <vg249@cornell.edu>
2019-10-10 15:10:29 -0400

    Resolved conflict after merge with develop 2

--- gobii-model/src/main/java/org/gobiiproject/gobiimodel/dto/base/DTOBase.java
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/dto/base/DTOBase.java
@@ -1,17 +1,9 @@
 package org.gobiiproject.gobiimodel.dto.base;
 
-<<<<<<< HEAD
-import org.codehaus.jackson.annotate.JsonIgnore;
-import org.gobiiproject.gobiimodel.dto.entity.annotations.GobiiEntityColumn;
-import org.gobiiproject.gobiimodel.dto.entity.annotations.GobiiEntityParam;
 import org.gobiiproject.gobiimodel.types.GobiiProcessType;
 
-import java.util.Date;
-=======
->>>>>>> develop
 import java.util.HashSet;
 import java.util.Set;
-import org.gobiiproject.gobiimodel.types.GobiiProcessType;
 
 /**
  * Created by Phil on 9/25/2016.
