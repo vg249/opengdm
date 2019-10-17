467f843913f95dc3eab4b06c84ad868ac5b35c3d
Vishnu <vg249@cornell.edu>
2019-10-17 12:21:27 -0400

    added cross origin annotation to BrAPI controller

--- gobii-web/src/main/java/org/gobiiproject/gobiiweb/controllers/BRAPIIControllerV1.java
+++ gobii-web/src/main/java/org/gobiiproject/gobiiweb/controllers/BRAPIIControllerV1.java
@@ -51,12 +51,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 
 /**
@@ -126,6 +121,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @Api()
 @RequestMapping(GobiiControllerType.SERVICE_PATH_BRAPI)
+@CrossOrigin
 public class BRAPIIControllerV1 {
 
     private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(BRAPIIControllerV1.class);
