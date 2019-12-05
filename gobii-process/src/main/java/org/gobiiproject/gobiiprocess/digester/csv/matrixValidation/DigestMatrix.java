b590f1e92462b7eb721476339a9351f4f3aef795
Joshua Lamos-Sweeney <jdl232@cornell.edu>
2019-12-05 13:12:34 -0500

    Consistenting and Oopifying DigestMatrix.java, a class that checks post-processed data before going into HDF5.

--- gobii-process/src/main/java/org/gobiiproject/gobiiprocess/digester/csv/matrixValidation/DigestMatrix.java
+++ gobii-process/src/main/java/org/gobiiproject/gobiiprocess/digester/csv/matrixValidation/DigestMatrix.java
@@ -1,50 +1,40 @@
 package org.gobiiproject.gobiiprocess.digester.csv.matrixValidation;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+
 import org.apache.commons.lang.StringUtils;
 import org.gobiiproject.gobiimodel.types.DataSetType;
 
 class DigestMatrix {
 
     static boolean validateDatasetList(int lineNumber, List<String> rowList, String type, MatrixErrorUtil matrixErrorUtil) {
-        List<String> allowedCharacters;
+        AllowedEntities allowedEntities;
         DataSetType dataSetType = DataSetType.valueOf(type);
         boolean returnStatus = true;
         switch (dataSetType) {
             case NUCLEOTIDE_2_LETTER:
             case IUPAC:
             case VCF:
-                allowedCharacters = initNucleotide2letterList();
+                allowedEntities = new AllowedEntitiesSet(new HashSet<String>(initNucleotide2letterList()));
                 break;
             case CO_DOMINANT_NON_NUCLEOTIDE:
-                allowedCharacters = initCoDominantList();
+                allowedEntities = new AllowedEntitiesSet(new HashSet<String>(initCoDominantList()));
                 break;
             case DOMINANT_NON_NUCLEOTIDE:
-                allowedCharacters = initDominantList();
+                allowedEntities = new AllowedEntitiesSet(new HashSet<String>(initDominantList()));
                 break;
           case NUCLEOTIDE_4_LETTER:
-               allowedCharacters = initNucleotide4letterList();
+               allowedEntities = new AllowedNucleotides(4);
+               break;
             case SSR_ALLELE_SIZE:
-                /*
-                 * since ssr data has only digits (upto 8)
-                 */
-                for (String element : rowList) {
-                    // Only accept numerical strings of exactly eight characters. No hetro SSR
-                    if (!(StringUtils.isNumeric(element) && element.length() == 8)) {
-                        matrixErrorUtil.setError("Validate Dataset Matrix. Invalid data found in post-processed matrix line: " + lineNumber + " Data:" + element);
-                        returnStatus = false;
-                    }
-                }
-                return returnStatus;
+                allowedEntities = new AllowedSSRAlleleSize(8);
             default:
                 matrixErrorUtil.setError("Validate Dataset Matrix. Invalid dataset type " + dataSetType);
                 return false;
         }
 
         for (String element : rowList)
-            if (element == null || element.equals("") || !allowedCharacters.contains(element)) {
+            if (element == null || element.equals("") || !allowedEntities.isAllowed(element)) {
                 matrixErrorUtil.setError("Validate Dataset Matrix Invalid data found in post-processed matrix line: " + lineNumber + " Data:" + element);
                 returnStatus = false;
             }
@@ -65,18 +55,6 @@ class DigestMatrix {
         }
         return elements;
     }
-  
-      private static List<String> initNucleotide4letterList() { \\TODO - fix for 4 letters
-        List<String> elements = new ArrayList<>(Arrays.asList("AA", "TT", "CC", "GG", "AT", "TA", "AG", "GA", "AC", "CA", "TG", "GT", "TC", "CT", "GC", "CG", "NN",
-                "++", "--", "+-", "-+", "AN", "NA", "CN", "NC", "GN", "NG", "TN", "NT"));
-        for (char c : "ACGTN+-".toCharArray()) {
-            elements.add(c + "+");
-            elements.add(c + "-");
-            elements.add("+" + c);
-            elements.add("-" + c);
-        }
-        return elements;
-    }
 
     private static List<String> initDominantList() {
         return Arrays.asList("0", "1", "N");
@@ -84,5 +62,53 @@ class DigestMatrix {
 
     private static List<String> initCoDominantList() {
         return Arrays.asList("0", "1", "2", "N");
+    }
+
+    private interface AllowedEntities{
+        boolean isAllowed(String entity);
+    }
+
+    private static class AllowedEntitiesSet implements AllowedEntities{
+        private Set<String> elements;
+        private AllowedEntitiesSet(Set<String> elements){
+            this.elements = elements;
+        }
+
+        @Override
+        public boolean isAllowed(String entity) {
+            return elements.contains(entity);
+        }
+    }
+    private static class AllowedNucleotides implements AllowedEntities{
+        private int numberOfElements;
+        private String allowedCharacters = "ACGTN+-";
+        private AllowedNucleotides(int numberOfElements){
+            this.numberOfElements = numberOfElements;
+        }
+        @Override
+        public boolean isAllowed(String entity) {
+            //There are the right number of elements
+            if(entity.length()!=numberOfElements) return false;
+            //Each element is a valid element
+            for(char c: entity.toCharArray()){
+                if(!allowedCharacters.contains(""+c)){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    private static class AllowedSSRAlleleSize implements AllowedEntities{
+        private int numberOfElements;
+        private AllowedSSRAlleleSize(int numberOfElements){
+            this.numberOfElements = numberOfElements;
+        }
+        @Override
+        public boolean isAllowed(String entity) {
+            //There are the right number of elements
+            if(entity.length()!=numberOfElements) return false;
+            //Right number of elements and is numeric
+            return StringUtils.isNumeric(entity);
+        }
     }
 }
