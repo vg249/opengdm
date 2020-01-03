9e4ce46e456127c1a35aa8ccf48cfe83e5d85ad2
ljc237 <ljc237@cornell.edu>
2020-01-03 10:31:29 -0500

    Changed back to whitelisting markers from white listing separators

--- gobii-process/src/test/java/org/gobiiproject/gobiiprocess/digester/HeplerFunctions/PGArrayTest.java
+++ gobii-process/src/test/java/org/gobiiproject/gobiiprocess/digester/HeplerFunctions/PGArrayTest.java
@@ -9,10 +9,10 @@ public class PGArrayTest {
 	@Test
 	public void testDelimiterSeparatedStringToPgArrayString() {
 		Assert.assertEquals("{\"\"+\"\",\"\"-\"\",\"\"z\"\"}", PGArray.delimiterSeparatedStringToPgArrayString("+/-/z"));
-		Assert.assertEquals("{\"\"+\"\",\"\"-\"\",\"\"z\"\"}", PGArray.delimiterSeparatedStringToPgArrayString("+|-|z"));
-		Assert.assertEquals("{\"\"+\"\",\"\"-\"\",\"\"z\"\"}", PGArray.delimiterSeparatedStringToPgArrayString("+\\-\\z"));
-		Assert.assertEquals("{\"\"+\"\",\"\"-\"\",\"\"z\"\"}", PGArray.delimiterSeparatedStringToPgArrayString("+,-,z"));
-		Assert.assertEquals("{\"\"+\"\",\"\"-\"\",\"\"z\"\"}", PGArray.delimiterSeparatedStringToPgArrayString("+;-;z"));
+		Assert.assertEquals("{\"\"A\"\",\"\"b\"\",\"\"Y\"\"}", PGArray.delimiterSeparatedStringToPgArrayString("A|b|Y"));
+		Assert.assertEquals("{\"\".\"\",\"\".\"\",\"\".\"\"}", PGArray.delimiterSeparatedStringToPgArrayString(".\\.\\."));
+		Assert.assertEquals("{\"\"h\"\",\"\"z\"\",\"\"k\"\"}", PGArray.delimiterSeparatedStringToPgArrayString("h,z,k"));
+		Assert.assertEquals("{\"\"A\"\",\"\"M\"\",\"\"Z\"\"}", PGArray.delimiterSeparatedStringToPgArrayString("A;M;Z"));
 		Assert.assertEquals("{\"\"+\"\",\"\"-\"\",\"\"z\"\"}", PGArray.delimiterSeparatedStringToPgArrayString("+:-:z"));
 		Assert.assertEquals("{\"\"+\"\",\"\"-\"\",\"\"z\"\"}", PGArray.delimiterSeparatedStringToPgArrayString("+/-\\z"));
 		Assert.assertEquals("{\"\"+\"\",\"\"-\"\",\"\"z\"\"}", PGArray.delimiterSeparatedStringToPgArrayString("+:-;z"));
