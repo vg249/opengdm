830369d13e8b38fa1df1a677b52ec89be7659d41
Vishnu Govindaraj <vg249@cornell.edu>
2020-02-18 13:56:53 +0530

    added getMapsetWithCountsById method implementation @MapsetBrapiImpl
    declared query strings as class fields so that they can be easily modified or reused if needed

--- gobii-sampletracking-dao/src/main/java/org/gobiiproject/gobiisampletrackingdao/MapsetDaoImpl.java
+++ gobii-sampletracking-dao/src/main/java/org/gobiiproject/gobiisampletrackingdao/MapsetDaoImpl.java
@@ -1,16 +1,19 @@
 package org.gobiiproject.gobiisampletrackingdao;
 
 import org.gobiiproject.gobiimodel.config.GobiiException;
+import org.gobiiproject.gobiimodel.entity.DnaRun;
 import org.gobiiproject.gobiimodel.entity.Mapset;
 import org.gobiiproject.gobiimodel.types.GobiiStatusLevel;
 import org.gobiiproject.gobiimodel.types.GobiiValidationStatusType;
 import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
 import org.hibernate.type.IntegerType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,27 +24,36 @@ public class MapsetDaoImpl implements MapsetDao {
 
     Logger LOGGER = LoggerFactory.getLogger(MapsetDaoImpl.class);
 
+    String mapsByExperimentIdListQueryString = "SELECT DISTINCT {mapset.*}, " +
+            "COUNT(DISTINCT linkage_group_id) AS linkage_group_count, " +
+            " COUNT(marker_id) AS marker_count " +
+            "FROM experiment " +
+            "INNER JOIN dataset USING(experiment_id) " +
+            "INNER JOIN marker ON(jsonb_exists(marker.dataset_marker_idx, CAST(dataset.dataset_id AS TEXT))) " +
+            "INNER JOIN marker_linkage_group USING(marker_id) " +
+            "INNER JOIN linkage_group USING(linkage_group_id) " +
+            "INNER JOIN mapset ON(mapset.mapset_id = linkage_group.map_id) " +
+            "WHERE (:experimentId IS NULL OR experiment.experiment_id = :experimentId) " +
+            "AND (:mapsetId IS NULL OR mapset.mapset_Id = :mapsetId) " +
+            "GROUP BY mapset.mapset_id;";
+
+    String mapsListQueryString = "SELECT {mapset.*}, COUNT(DISTINCT linkage_group_id) AS linkage_group_count, " +
+            "COUNT(marker_id) AS marker_count " +
+            "FROM mapset LEFT OUTER JOIN linkage_group ON(mapset.mapset_id = linkage_group.map_id) " +
+            "LEFT OUTER JOIN marker_linkage_group USING(linkage_group_id) " +
+            "WHERE (:mapsetId IS NULL OR mapset.mapset_Id = :mapsetId) " +
+            "GROUP BY mapset.mapset_id;";
+
+    String mapsQueryString = "";
 
     @PersistenceContext
     protected EntityManager em;
 
-    public List<Mapset> getMapsetsWithCountsByExperimentId(
-            Integer pageSize, Integer rowOffset,
+    public List<Mapset> getMapsetsWithCounts(
+            Integer pageSize, Integer rowOffset, Integer mapsetId,
             Integer experimentId) throws GobiiException {
 
         List<Mapset> mapsetsWithCounts = new ArrayList<>();
-
-        String queryString = "SELECT DISTINCT {mapset.*}, COUNT(linkage_group_id) AS linkage_group_count, " +
-                " COUNT(DISTINCT marker_id) AS marker_count " +
-                "FROM experiment " +
-                "INNER JOIN dataset USING(experiment_id) " +
-                "INNER JOIN marker ON(jsonb_exists(marker.dataset_marker_idx, CAST(dataset.dataset_id AS TEXT))) " +
-                "INNER JOIN marker_linkage_group USING(marker_id) " +
-                "INNER JOIN linkage_group USING(linkage_group_id) " +
-                "INNER JOIN mapset ON(mapset.mapset_id = linkage_group.map_id) " +
-                "WHERE (:experimentId IS NULL OR experiment.experiment_id = :experimentId)" +
-                "GROUP BY mapset.mapset_id;";
-
 
         try {
 
@@ -49,21 +61,33 @@ public class MapsetDaoImpl implements MapsetDao {
 
             session.enableFetchProfile("mapset-typecv");
 
-            List<Object[]> resultTuples = session.createNativeQuery(queryString)
+            String queryString = mapsListQueryString;
+
+            if(experimentId != null) {
+                queryString = mapsByExperimentIdListQueryString;
+            }
+
+            NativeQuery query = session.createNativeQuery(queryString)
                     .addEntity("mapset", Mapset.class)
                     .addScalar("linkage_group_count", IntegerType.INSTANCE)
                     .addScalar("marker_count", IntegerType.INSTANCE)
-                    .setParameter("experimentId", experimentId, IntegerType.INSTANCE)
+                    .setParameter("mapsetId", mapsetId, IntegerType.INSTANCE)
                     .setFetchSize(pageSize)
-                    .setFirstResult(rowOffset)
-                    .list();
+                    .setFirstResult(rowOffset);
+
+            if(experimentId != null) {
+                query.setParameter("experimentId", experimentId);
+            }
+
+            List<Object[]> resultTuples = query.list();
 
 
             for(Object[] tuple : resultTuples) {
 
                 Mapset mapset = (Mapset) tuple[0];
+
                 mapset.setLinkageGroupCount((Integer) tuple[1]);
-                mapset.setMarkerCount((Integer) tuple[1]);
+                mapset.setMarkerCount((Integer) tuple[2]);
 
                 mapsetsWithCounts.add(mapset);
 
@@ -83,4 +107,43 @@ public class MapsetDaoImpl implements MapsetDao {
         }
 
     }
+
+
+  @Override
+   public Mapset getMapsetWithCountsById(Integer mapsetId) {
+      try {
+
+          List<Mapset> mapsetsById = this.getMapsetsWithCounts(null, null, mapsetId, null);
+
+          if (mapsetsById.size() > 1) {
+
+              LOGGER.error("More than one duplicate entries found.");
+
+              throw new GobiiDaoException(GobiiStatusLevel.ERROR,
+                      GobiiValidationStatusType.NONE,
+                      "More than one dnarun entity exists for the same Id");
+
+          } else if (mapsetsById.size() == 0) {
+              throw new GobiiDaoException(GobiiStatusLevel.ERROR,
+                      GobiiValidationStatusType.ENTITY_DOES_NOT_EXIST,
+                      "Dna run Entity for given id does not exist");
+          }
+
+          return mapsetsById.get(0);
+
+      }
+      catch(GobiiException ge) {
+          throw ge;
+      }
+      catch (Exception e) {
+
+          LOGGER.error(e.getMessage(), e);
+
+          throw new GobiiDaoException(GobiiStatusLevel.ERROR,
+                  GobiiValidationStatusType.UNKNOWN,
+                  e.getMessage() + " Cause Message: " + e.getCause().getMessage());
+
+      }
+
+  }
 }
