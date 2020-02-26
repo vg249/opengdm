13acf7e668019fb285fe37f3d1218e0ccf17c786
vg249 <vg249@cornell.edu>
2020-02-26 14:32:17 -0500

    added MapsetDao tests

--- gobii-sampletracking-dao/src/test/java/org/gobiiproject/gobiisampletrackingdao/MapsetDaoTest.java
+++ gobii-sampletracking-dao/src/test/java/org/gobiiproject/gobiisampletrackingdao/MapsetDaoTest.java
@@ -1,4 +1,44 @@
 package org.gobiiproject.gobiisampletrackingdao;
 
+import org.gobiiproject.gobiimodel.entity.Mapset;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/spring/test-config.xml"})
 public class MapsetDaoTest {
+
+    @Autowired
+    private MapsetDao mapsetDao;
+
+    @Test
+    public void getMapsetsTest() {
+
+        Integer testPageSize = 100;
+
+        List<Mapset> mapsets = mapsetDao.getMapsetsWithCounts(testPageSize, 0,
+                null, null);
+
+        assertTrue("getMapsets page size condition fails",
+                mapsets.size() > 0 && mapsets.size() <= testPageSize);
+
+    }
+
+    @Test
+    public void getMapsetByIdTest() {
+
+        Integer testPageSize = 100;
+
+        Mapset mapset = mapsetDao.getMapsetWithCountsById(2);
+
+        assertTrue("Get Mapset By Id fails",mapset.getMapsetId() == 2);
+
+    }
 }
