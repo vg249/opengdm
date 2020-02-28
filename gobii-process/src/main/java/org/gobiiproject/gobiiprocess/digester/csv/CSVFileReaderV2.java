bb4eab6330e4382738ca078eedbf4a83edddda99
Joshua Lamos-Sweeney <jdl232@cornell.edu>
2020-02-28 15:24:09 -0500

    Fixing unrelated issues.... because I need to get to the point to reproduce the related issue.... because this is on fire. Sorry maintenance programmer

--- gobii-process/src/main/java/org/gobiiproject/gobiiprocess/digester/csv/CSVFileReaderV2.java
+++ gobii-process/src/main/java/org/gobiiproject/gobiiprocess/digester/csv/CSVFileReaderV2.java
@@ -401,7 +401,12 @@ public class CSVFileReaderV2 extends CSVFileReaderInterface {
                 case CSV_ROW:
                     appendTabToOutput(outputLine, column);
                     //TODO- this is also super bad, using remove on a long array list
-                    outputLine.append(processedInstruction.getRequiredRows().get(rowNo).remove(0));
+                    try {
+                        outputLine.append(processedInstruction.getRequiredRows().get(rowNo).remove(0));
+                    }catch(IndexOutOfBoundsException e){
+                        //TODO - found working on validation logic - This case can OOB if any row is jagged.
+                        Logger.logWarning("CSVInstructionProcessor","Missing line for data, zero width added to output string");
+                    }
                     rowNo++;
                     break;
                 case CSV_COLUMN:
