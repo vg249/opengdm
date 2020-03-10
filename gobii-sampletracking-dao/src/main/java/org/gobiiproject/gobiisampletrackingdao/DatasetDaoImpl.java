5c6a877a4c2cec195de0e5e4ea3f8fc169999e48
vg249 <vg249@cornell.edu>
2020-03-10 16:48:18 -0400

    added implementation for getDatasetWithAnalysesAndCounts method and refactored getDatasets

--- gobii-sampletracking-dao/src/main/java/org/gobiiproject/gobiisampletrackingdao/DatasetDaoImpl.java
+++ gobii-sampletracking-dao/src/main/java/org/gobiiproject/gobiisampletrackingdao/DatasetDaoImpl.java
@@ -8,6 +8,7 @@ import org.gobiiproject.gobiimodel.types.GobiiStatusLevel;
 import org.gobiiproject.gobiimodel.types.GobiiValidationStatusType;
 import org.hibernate.Session;
 import org.hibernate.type.IntegerType;
+import org.hibernate.type.StringType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,9 +30,6 @@ public class DatasetDaoImpl implements DatasetDao {
 
     @PersistenceContext
     protected EntityManager em;
-
-    final int defaultPageSize = 1000;
-
 
     /**
      * @param pageSize - size of the page
@@ -59,18 +57,9 @@ public class DatasetDaoImpl implements DatasetDao {
             CriteriaQuery<Dataset> criteriaQuery = criteriaBuilder.createQuery(Dataset.class);
 
             Root<Dataset> datasetRoot = criteriaQuery.from(Dataset.class);
+            criteriaQuery.select(datasetRoot);
 
-            Join<Object, Object> callingAnalysis = (Join<Object, Object>) datasetRoot.fetch(
-                    "callingAnalysis",
-                    JoinType.LEFT);
-
-            //Inner join as there is referential integrity between experiment and dataset
             Join<Object, Object> experiment = (Join<Object, Object>) datasetRoot.fetch("experiment");
-            Join<Object, Object> job = (Join<Object, Object>) datasetRoot.fetch("job", JoinType.LEFT);
-
-            Join<Object, Object> typeCv = (Join<Object, Object>) datasetRoot.fetch("type", JoinType.LEFT);
-
-            Join<Object, Object> statusCv = (Join<Object, Object>) datasetRoot.fetch("status", JoinType.LEFT);
 
             if(datasetId != null) {
                 predicates.add(criteriaBuilder.equal(datasetRoot.get("datasetId"), datasetId));
@@ -87,6 +76,8 @@ public class DatasetDaoImpl implements DatasetDao {
             if(experimentName != null) {
                 predicates.add(criteriaBuilder.equal(experiment.get("experimentName"), experimentName));
             }
+
+            criteriaQuery.where(predicates.toArray(new Predicate[]{}));
 
             datasets = em.createQuery(criteriaQuery)
                     .setFirstResult(rowOffset)
@@ -108,7 +99,6 @@ public class DatasetDaoImpl implements DatasetDao {
         }
     }
 
-
     /**
      * Gets the dataset entity by dataset id.
      * @param datasetId - Dataset Id
@@ -118,9 +108,12 @@ public class DatasetDaoImpl implements DatasetDao {
     @Transactional
     public Dataset getDatasetById(Integer datasetId) throws GobiiException {
 
+        Objects.requireNonNull(datasetId, "datasetId : Required non null");
+
         try {
 
-            List<Dataset> datasetsById = this.getDatasets(0, 1000,
+            //Overload the getDatasets
+            List<Dataset> datasetsById = this.getDatasets(1000, 0,
                     datasetId, null,
                     null, null);
 
@@ -153,6 +146,93 @@ public class DatasetDaoImpl implements DatasetDao {
 
     }
 
+    /**
+     * Returns a list of object tuple with Dataset entity left joined with analysis entities
+     * and their respective marker and dnarun count.
+     *
+     * @param pageSize - number of dataset entities to be fetched
+     * @param rowOffset - row offset for database list
+     * @param datasetId - filter by dataset id
+     * @param datasetName - filter by dataset name
+     * @param experimentId - filter by experiment id
+     * @param experimentName - filter by experiment name
+     * @return List<Object[]> list of object tuple with,
+     * Object[0] - Dataset Entity
+     * Object[1] - Analysis Entity related to database. Joined using analyses column
+     * Object[2] - markers count for each database entity
+     * Object[3] - dnaruns count for each database entity
+     */
+    @Override
+    @Transactional
+    public List<Object[]> getDatasetsWithAnalysesAndCounts(
+          Integer pageSize, Integer rowOffset,
+          Integer datasetId, String datasetName,
+          Integer experimentId, String experimentName) {
 
+
+        String queryString = "WITH ds AS (" +
+                "SELECT * " +
+                "FROM dataset " +
+                "WHERE (:datasetId IS NULL OR dataset_id = :datasetId) " +
+                "AND (:datasetName IS NULL OR dataset.name = :datasetName) " +
+                "LIMIT :pageSize OFFSET :rowOffset) " +
+                "SELECT {ds.*} , {anas.*}, {experiment.*}, {callinganalysis.*}, {job.*}, " +
+                "{typeCv.*}, {statusCv.*}, " +
+                "(SELECT gettotalmarkersindataset " +
+                "FROM gettotalmarkersindataset(CAST(ds.dataset_id AS TEXT))) " +
+                "AS marker_count, " +
+                "(SELECT gettotaldnarunsindataset " +
+                "FROM gettotaldnarunsindataset(CAST(ds.dataset_id AS TEXT))) " +
+                "AS dnarun_count " +
+                "FROM ds " +
+                "INNER JOIN experiment AS experiment ON(" +
+                "   (ds.experiment_id = experiment.experiment_id) " +
+                "   AND (:experimentId IS NULL OR experiment.experiment_id = :experimentId) " +
+                "   AND (:experimentName IS NULL OR experiment.name = :experimentName) " +
+                ") " +
+                "LEFT JOIN analysis AS anas ON(anas.analysis_id = ANY(ds.analyses)) " +
+                "LEFT JOIN analysis AS callinganalysis ON(callinganalysis.analysis_id = ds.callinganalysis_id) " +
+                "LEFT JOIN job USING(job_id) " +
+                "LEFT JOIN cv typeCv ON(job.type_id = typeCv.cv_id) " +
+                "LEFT JOIN cv statusCv ON(job.status = statusCv.cv_id) ";
+
+        Objects.requireNonNull(pageSize, "pageSize: Required non null");
+        Objects.requireNonNull(rowOffset, "rowOffset: Required non null");
+
+        try {
+
+            Session session = em.unwrap(Session.class);
+
+            List<Object[]> resultTupleList = session.createNativeQuery(queryString)
+                    .addEntity("ds", Dataset.class)
+                    .addEntity("anas", Analysis.class)
+                    .addJoin("experiment", "ds.experiment")
+                    .addJoin("job", "ds.job")
+                    .addJoin("callinganalysis", "ds.callingAnalysis")
+                    .addJoin("typeCv", "job.type")
+                    .addJoin("statusCv", "job.status")
+                    .addScalar("marker_count", IntegerType.INSTANCE)
+                    .addScalar("dnarun_count", IntegerType.INSTANCE)
+                    .setParameter("pageSize", pageSize, IntegerType.INSTANCE)
+                    .setParameter("rowOffset", rowOffset, IntegerType.INSTANCE)
+                    .setParameter("datasetId", datasetId, IntegerType.INSTANCE)
+                    .setParameter("experimentId", experimentId, IntegerType.INSTANCE)
+                    .setParameter("datasetName", datasetName, StringType.INSTANCE)
+                    .setParameter("experimentName", experimentName, StringType.INSTANCE)
+                    .list();
+
+            return resultTupleList;
+
+        }
+        catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+
+            throw new GobiiDaoException(GobiiStatusLevel.ERROR,
+                    GobiiValidationStatusType.UNKNOWN,
+                    e.getMessage() + " Cause Message: " + e.getCause().getMessage());
+
+        }
+
+    }
 
 }
