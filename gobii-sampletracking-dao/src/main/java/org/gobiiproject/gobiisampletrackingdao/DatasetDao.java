5b0c6e4173e6931ec799efc05947239a2368d26e
vg249 <vg249@cornell.edu>
2020-03-10 16:47:15 -0400

    added new method to getDatasets with associated analyses as Object tuple @DataSetDao interface

--- gobii-sampletracking-dao/src/main/java/org/gobiiproject/gobiisampletrackingdao/DatasetDao.java
+++ gobii-sampletracking-dao/src/main/java/org/gobiiproject/gobiisampletrackingdao/DatasetDao.java
@@ -15,4 +15,7 @@ public interface DatasetDao {
 
     Dataset getDatasetById(Integer datasetId);
 
+    List<Object[]> getDatasetsWithAnalysesAndCounts(Integer pageSize, Integer rowOffset,
+                                                    Integer datasetId, String datasetName,
+                                                    Integer experimentId, String experimentName);
 }
