faa3c3f32dccc350bf9547489a4f7ccbf0330fb0
vg249 <vg249@cornell.edu>
2020-03-12 14:03:57 -0400

    Added content-type to the Authentication filter Raw HTTP Response

--- gobii-web/src/main/java/org/gobiiproject/gobiiweb/automation/ControllerUtils.java
+++ gobii-web/src/main/java/org/gobiiproject/gobiiweb/automation/ControllerUtils.java
@@ -3,6 +3,7 @@ package org.gobiiproject.gobiiweb.automation;
 import javax.servlet.http.HttpServletResponse;
 import org.gobiiproject.gobiiapimodel.payload.Header;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 /**
  * Created by Phil on 9/23/2016.
@@ -27,6 +28,7 @@ public class ControllerUtils {
                                         String message) throws Exception {
 
         httpResponse.setStatus(httpServletResponse);
+        httpResponse.setContentType(MediaType.APPLICATION_JSON.toString());
         httpResponse.getOutputStream().print(message);
         httpResponse.getOutputStream().flush();
         httpResponse.getOutputStream().close();
