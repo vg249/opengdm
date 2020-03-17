c7c101330beeba20757a3b8a89112ecd37be92a5
vg249 <vg249@cornell.edu>
2020-03-17 18:34:59 -0400

    added javax Validator to validate atleast one bean fields is not null and list fields are not empty

--- gobii-model/src/main/java/org/gobiiproject/gobiimodel/validators/CheckAtLeastOneNotNullOrEmpty.java
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/validators/CheckAtLeastOneNotNullOrEmpty.java
@@ -1,4 +1,80 @@
 package org.gobiiproject.gobiimodel.validators;
 
-public interface CheckAtLeastOneNotNullOrEmpty {
+
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.util.List;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import org.apache.commons.beanutils.PropertyUtils;
+
+/**
+ * reference : "https://stackoverflow.com/questions/12211734/
+ *              hibernate-validation-annotation-validate-that-at-least-one-field-is-not-null"
+ *
+ */
+@Target( { TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = CheckAtLeastOneNotNullOrEmpty.CheckAtLeastOneNotNullOrEmptyValidator.class)
+@Documented
+public @interface CheckAtLeastOneNotNullOrEmpty {
+
+    String message() default "Empty Search Query";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String[] fieldNames();
+
+    class CheckAtLeastOneNotNullOrEmptyValidator implements ConstraintValidator<CheckAtLeastOneNotNullOrEmpty, Object> {
+
+        private String[] fieldNames;
+
+        public void initialize(CheckAtLeastOneNotNullOrEmpty constraintAnnotation) {
+            this.fieldNames = constraintAnnotation.fieldNames();
+        }
+
+        public boolean isValid(Object object, ConstraintValidatorContext constraintContext) {
+
+
+            if (object == null)
+                return true;
+
+            try {
+
+                for (String fieldName:fieldNames){
+
+                    Object property = PropertyUtils.getProperty(object, fieldName);
+
+                    if (property == null) {
+                        return false;
+                    }
+                    else if(property instanceof List<?> && ((List<Object>)property).size() == 0) {
+                        return false;
+                    }
+                    else {
+                        return true;
+                    }
+
+                }
+
+                return false;
+
+            } catch (Exception e) {
+                return false;
+            }
+        }
+
+    }
+
+
 }
