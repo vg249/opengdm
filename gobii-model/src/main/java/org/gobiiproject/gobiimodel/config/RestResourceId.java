870ce1f97e3fc8fbf3886fe398e83e81cf0e3dea
vg249 <vg249@cornell.edu>
2020-03-25 22:36:14 -0400

    Added Resource Ids for new Brapi calls

--- gobii-model/src/main/java/org/gobiiproject/gobiimodel/config/RestResourceId.java
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/config/RestResourceId.java
@@ -69,8 +69,39 @@ public enum RestResourceId {
     BRAPI_STUDIES_SEARCH("studies-search", ServerType.BRAPI),
     BRAPI_STUDIES("studies", ServerType.BRAPI),
     BRAPI_CALLS("calls", ServerType.BRAPI),
+
+
+    BRAPI_SERVER_INFO("serverinfo", ServerType.BRAPI),
+
     BRAPI_MAPS_CALLS("maps", ServerType.BRAPI),
-    BRAPI_CALLSETS("callsets", ServerType.BRAPI);
+
+    BRAPI_MARKER_POSITIONS("markerpositions", ServerType.BRAPI),
+
+    BRAPI_CALLSETS("callsets", ServerType.BRAPI),
+
+    BRAPI_CALLSETS_BY_ID("callsets/{callSetDbId}", ServerType.BRAPI),
+
+    BRAPI_VARIANTS("variants", ServerType.BRAPI),
+
+    BRAPI_VARIANTS_BY_ID("variants/{variantDbId}", ServerType.BRAPI),
+
+    BRAPI_VARIANTSETS("variantsets", ServerType.BRAPI),
+
+    BRAPI_GENOTYPES_BY_VARIANTSET(
+            "variantsets/{variantSetDbId}/calls",
+            ServerType.BRAPI),
+
+    BRAPI_DOWNLOAD_GENOTYPES_BY_VARIANTSET(
+            "variantsets/{variantSetDbId}/calls/download",
+            ServerType.BRAPI),
+
+    BRAPI_VARIANTS_BY_VARIANTSET(
+            "variantsets/{variantSetDbId}/variants",
+            ServerType.BRAPI),
+
+    BRAPI_CALLSETS_BY_VARIANTSET(
+            "variantsets/{variantSetDbId}/callsets",
+            ServerType.BRAPI);
 
     private String requestPath;
     private ServerType serverType;
