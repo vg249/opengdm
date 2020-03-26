66e93fad6d6b815418df32ab29fcf09df8e69ed7
vg249 <vg249@cornell.edu>
2020-03-25 22:51:43 -0400

    add ExperimentDao interface with getExperiments method
    Implementation of getExperiments method @ExperimentServiceImpl

--- gobii-sampletracking-dao/src/main/java/org/gobiiproject/gobiisampletrackingdao/ExperimentDao.java
+++ gobii-sampletracking-dao/src/main/java/org/gobiiproject/gobiisampletrackingdao/ExperimentDao.java
@@ -1,4 +1,12 @@
 package org.gobiiproject.gobiisampletrackingdao;
 
+import org.gobiiproject.gobiimodel.entity.Experiment;
+
+import java.util.List;
+
 public interface ExperimentDao {
+
+    List<Experiment> getExperiments(Integer pageSize, Integer rowOffset,
+                                    Integer projectId);
+
 }
