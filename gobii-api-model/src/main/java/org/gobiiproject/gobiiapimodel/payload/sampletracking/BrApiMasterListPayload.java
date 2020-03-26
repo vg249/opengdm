c8b2788dfcacb3f14e8c1ca53997efb42ae3231b
vg249 <vg249@cornell.edu>
2020-03-25 22:30:08 -0400

    added setServerInfo method to add list of available brapi calls and their details @BrapiMasterListPayload

--- gobii-api-model/src/main/java/org/gobiiproject/gobiiapimodel/payload/sampletracking/BrApiMasterListPayload.java
+++ gobii-api-model/src/main/java/org/gobiiproject/gobiiapimodel/payload/sampletracking/BrApiMasterListPayload.java
@@ -17,13 +17,16 @@ public class BrApiMasterListPayload<T>  extends BrApiMasterPayload {
         this.result.setData(listData);
     }
 
-    public BrApiMasterListPayload(List<T> listData, Integer pageSize, Integer currentPage) {
+
+    public BrApiMasterListPayload(List<T> listData, Integer pageSize,
+                                  Integer currentPage) {
         this.result.setData(listData);
         this.getMetadata().getPagination().setPageSize(pageSize);
         this.getMetadata().getPagination().setCurrentPage(currentPage);
     }
 
-    public BrApiMasterListPayload(List<T> listData, Integer pageSize, String nextPageToken) {
+    public BrApiMasterListPayload(List<T> listData, Integer pageSize,
+                                  String nextPageToken) {
 
         this.result.setData(listData);
         this.getMetadata().getPagination().setPageSize(pageSize);
@@ -39,5 +42,11 @@ public class BrApiMasterListPayload<T>  extends BrApiMasterPayload {
         this.result = result;
     }
 
+    public void setServerInfo(List<T> listCalls, Integer pageSize,
+                              Integer currentPage) {
+        this.result.setCalls(listCalls);
+        this.getMetadata().getPagination().setPageSize(pageSize);
+        this.getMetadata().getPagination().setCurrentPage(currentPage);
+    }
 
 }
---
+++ gobii-sampletracking-dao/src/main/java/org/gobiiproject/gobiisampletrackingdao/ExperimentDaoImpl.java
@@ -0,0 +1,4 @@
+package org.gobiiproject.gobiisampletrackingdao;
+
+public class ExperimentDaoImpl {
+}
