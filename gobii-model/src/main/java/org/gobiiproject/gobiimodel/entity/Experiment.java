abbd67380563a0a4db2cb872215d0298ba574ab9
Rodolfo N. Duldulao, Jr <rnduldulaojr@gmail.com>
2020-03-31 23:14:42 +0800

    Missing import fix

--- gobii-model/src/main/java/org/gobiiproject/gobiimodel/entity/Experiment.java
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/entity/Experiment.java
@@ -5,7 +5,6 @@
  * 
  */
 
-
 package org.gobiiproject.gobiimodel.entity;
 
 import javax.persistence.*;
@@ -15,6 +14,7 @@ import org.gobiiproject.gobiimodel.entity.Cv;
 import org.gobiiproject.gobiimodel.entity.Project;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @Entity
