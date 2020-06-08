6b50f075e7dea4f6e78f33c365fe5cf5a085b523
vg249 <vg249@cornell.edu>
2020-06-08 18:26:45 -0400

    added test methods for callset service

--- gobii-domain/src/test/java/org/gobiiproject/gobidomain/services/brapi/CallSetServiceImplTest.java
+++ gobii-domain/src/test/java/org/gobiiproject/gobidomain/services/brapi/CallSetServiceImplTest.java
@@ -1,8 +1,11 @@
 package org.gobiiproject.gobidomain.services.brapi;
 
-import org.apache.commons.lang.RandomStringUtils;
+import org.gobiiproject.gobiimodel.cvnames.CvGroup;
 import org.gobiiproject.gobiimodel.dto.brapi.CallSetDTO;
-import org.gobiiproject.gobiimodel.entity.DnaRun;
+import org.gobiiproject.gobiimodel.dto.system.PagedResult;
+import org.gobiiproject.gobiimodel.entity.Cv;
+import org.gobiiproject.gobiisampletrackingdao.CvDao;
+import org.gobiiproject.gobiisampletrackingdao.CvDaoImpl;
 import org.gobiiproject.gobiisampletrackingdao.DnaRunDaoImpl;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +17,10 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by VCalaminos on 7/18/2019.
@@ -29,39 +36,70 @@ public class CallSetServiceImplTest {
     @Mock
     private DnaRunDaoImpl dnaRunDao;
 
+    @Mock
+    private CvDaoImpl cvDao;
+
     Random random = new Random();
+
+    MockSetup mockSetup;
+
+    final Integer pageSize = 10;
+    final Integer pageNum = 0;
 
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
+        mockSetup  = new MockSetup();
     }
-
-    private List<DnaRun> createMockDnaRuns(int numOfDnaRuns) {
-
-        List<DnaRun> dnaRuns = new ArrayList<>();
-
-        for(int i = 0; i < numOfDnaRuns; i++) {
-            DnaRun dnaRun = new DnaRun();
-            dnaRun.setDnaRunName(RandomStringUtils.random(7, true, true));
-
-        }
-
-        return dnaRuns;
-    }
-
 
     @Test
     public void getCallsets() throws Exception {
 
-        //List<CallSetDTO> callsetsMock = new ArrayList<>();
+        mockSetup.createMockDnaRuns(pageSize);
+
+        when (
+            dnaRunDao.getDnaRuns(any(Integer.TYPE), any(Integer.TYPE),
+                any(Integer.TYPE), any(String.class),
+                any(Integer.TYPE), any(Integer.TYPE),
+                any(Integer.TYPE), any(String.class),
+                any(Integer.TYPE), any(String.class))
+        ).thenReturn(mockSetup.mockDnaRuns);
+
+
+        when (cvDao.getCvListByCvGroup(
+            CvGroup.CVGROUP_DNASAMPLE_PROP.getCvGroupName(), null)).thenReturn(
+                new ArrayList<>()
+        );
+
+        when (cvDao.getCvListByCvGroup(
+            CvGroup.CVGROUP_GERMPLASM_PROP.getCvGroupName(), null))
+            .thenReturn(mockSetup.mockGermplasmProps);
+
+
+        PagedResult<CallSetDTO>  callSetsPageResult =
+            callSetBrapiService.getCallSets(
+                pageSize, 0, null, new CallSetDTO());
+
+        assertEquals("Page Size mismatch",
+            pageSize,
+            callSetsPageResult.getCurrentPageSize());
+
+        assertEquals("Wrong page number",
+            pageNum,
+            callSetsPageResult.getCurrentPageNum());
+
+        for(int i = 0; i < testPageSize; i++) {
+            assertEquals("CallSetId : DnaRunId mismatch",
+                mockSetup.mockDnaRuns.get(i).getDnaRunId(),
+                callSetsPageResult.getResult().get(i).getCallSetDbId());
+            assertEquals("CallSetName : DnaRunName mismacth",
+                mockSetup.mockDnaRuns.get(i).getDnaRunName(),
+                callSetsPageResult.getResult().get(i).getCallSetName());
+        }
 
 
 
     }
 
-    @Test
-    public void getCallsetById() throws Exception {
-
-    }
 
 }
