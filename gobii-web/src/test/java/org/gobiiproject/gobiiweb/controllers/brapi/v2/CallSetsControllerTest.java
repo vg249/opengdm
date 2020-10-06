c55157308b67ccb9dbf22b91725dec7526f9cf15
vg249 <vg249@cornell.edu>
2020-10-05 21:44:32 -0400

    added new getCallSetsWithNoParamsTest @CallSetControllerTest

--- gobii-web/src/test/java/org/gobiiproject/gobiiweb/controllers/brapi/v2/CallSetsControllerTest.java
+++ gobii-web/src/test/java/org/gobiiproject/gobiiweb/controllers/brapi/v2/CallSetsControllerTest.java
@@ -5,18 +5,29 @@ import org.apache.commons.lang.RandomStringUtils;
 import org.gobiiproject.gobiidomain.services.brapi.CallSetService;
 import org.gobiiproject.gobiidomain.services.brapi.GenotypeCallsService;
 import org.gobiiproject.gobiimodel.dto.brapi.CallSetDTO;
+import org.gobiiproject.gobiimodel.dto.system.PagedResult;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebAppConfiguration
 public class CallSetsControllerTest {
@@ -40,7 +51,9 @@ public class CallSetsControllerTest {
         this.mockMvc = MockMvcBuilders.standaloneSetup(callSetsController).build();
     }
 
-    private List<CallSetDTO> getMockCallSets(int pageSize) {
+    private PagedResult<CallSetDTO> getMockCallSets(int pageSize, int pageNum) {
+
+        PagedResult<CallSetDTO> pagedResult = new PagedResult<>();
 
         List<CallSetDTO> mockCallSets = new ArrayList<>();
 
@@ -58,13 +71,67 @@ public class CallSetsControllerTest {
             mockCallSets.add(callSetDTO);
         }
 
-        return mockCallSets;
+        pagedResult.setCurrentPageNum(pageNum);
+        pagedResult.setCurrentPageSize(mockCallSets.size());
+        pagedResult.setResult(mockCallSets);
+
+        return pagedResult;
     }
 
     @Test
-    public void getCallSetsTest() throws Exception {
+    public void getCallSetWithPageAndPageSizeTest() throws Exception {
+
         Integer pageSize = 10;
+        Integer pageNum = 4;
+
+        when(
+            callSetService.getCallSets(eq(pageSize), eq(pageNum),
+                any(), any())
+        ).thenReturn(getMockCallSets(pageSize, pageNum));
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.get(
+                "/gobii-dev/brapi/v2/callsets")
+                .param("pageSize",  pageSize.toString())
+                .param("page",  pageNum.toString())
+                .contextPath("/gobii-dev")).andDo(print())
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.metadata").exists())
+            .andExpect(jsonPath("$.metadata.pagination").exists())
+            .andExpect(jsonPath("$.metadata.pagination.currentPage").exists())
+            .andExpect(jsonPath("$.metadata.pagination.pageSize").value(pageSize))
+            .andExpect(jsonPath("$.metadata.pagination.currentPage").value(pageNum));
+    }
+
+    @Test
+    public void getCallSetsWithNoParams() throws Exception {
+
+        Integer defaultPageSize = 1000;
+        Integer defaultPageNum = 0;
+
+        Integer totalCallSets = 10;
+
+        when(
+            callSetService.getCallSets(eq(defaultPageSize), eq(defaultPageNum),
+                any(), any())
+        ).thenReturn(getMockCallSets(totalCallSets, defaultPageNum));
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.get(
+                "/gobii-dev/brapi/v2/callsets")
+                .contextPath("/gobii-dev")).andDo(print())
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.metadata").exists())
+            .andExpect(jsonPath("$.metadata.pagination").exists())
+            .andExpect(jsonPath("$.metadata.pagination.currentPage").exists())
+            .andExpect(jsonPath("$.metadata.pagination.pageSize").value(totalCallSets))
+            .andExpect(jsonPath("$.metadata.pagination.currentPage").value(defaultPageNum));
 
     }
+
+
+
 
 }
