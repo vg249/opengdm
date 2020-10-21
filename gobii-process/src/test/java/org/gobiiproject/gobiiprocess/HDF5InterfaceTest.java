de01b7c86db512813f556c61ccbbb6fafd09e94a
Joshua Lamos-Sweeney <jdl232@cornell.edu>
2020-10-21 00:36:07 -0400

    Adding the updates to this method before remerge to develop, worked on in wrong branch

---
+++ gobii-process/src/test/java/org/gobiiproject/gobiiprocess/HDF5InterfaceTest.java
@@ -0,0 +1,83 @@
+package org.gobiiproject.gobiiprocess;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.*;
+
+public class HDF5InterfaceTest {
+    @Test
+    public void testCollateFiles() throws IOException {
+        File t1 = new File("testfile1");
+        File t2 = new File("testfile2");
+        File t3 = new File("testfile3");
+        File out = new File("testfileout");
+
+        try (FileWriter w1 = new FileWriter(t1);FileWriter w2 = new FileWriter(t2);FileWriter w3 = new FileWriter(t3)) {
+            w1.write("Derp");
+            w1.write(System.lineSeparator());
+            w1.write("Derp2");
+            w2.write("Derp3");
+            w2.write(System.lineSeparator());
+            w2.write("Derp4");
+            w3.write("Derp5");
+            w3.write(System.lineSeparator());
+            w3.write("Derp6");
+            w3.write(System.lineSeparator());
+            w3.write("Derp7");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        HDF5Interface.coallateFiles("testfile1,testfile2,testfile3","\t","testfileout");
+
+        BufferedReader outReader = new BufferedReader(new FileReader(out));
+        String line1 = outReader.readLine();
+        String line2 = outReader.readLine();
+        String line3 = outReader.readLine();
+
+        Assert.assertEquals("Derp\tDerp3\tDerp5",line1);
+        Assert.assertEquals("Derp2\tDerp4\tDerp6",line2);
+        Assert.assertEquals("Derp7",line3);
+
+        t1.delete();
+        t2.delete();
+        t3.delete();
+        out.delete();
+    }
+    @Test
+    public void testEncodeDecode() throws IOException {
+        File input = new File("encodeinput");
+        File encoded = new File("encodeddata");
+        File out = new File("decodedinput");
+
+        try (FileWriter inputwriter = new FileWriter(input)) {
+            inputwriter.write("A/C\tG/T\tN/N");
+            inputwriter.write(System.lineSeparator());
+            inputwriter.write("ACAT/GAG\t/\tA/A");
+            inputwriter.write(System.lineSeparator());
+            inputwriter.write("A/ACAT\tG/GAG\tA/C");
+            inputwriter.write(System.lineSeparator());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        HDF5Interface.coallateFiles("testfile1,testfile2,testfile3","\t","testfileout");
+
+        BufferedReader outReader = new BufferedReader(new FileReader(out));
+        String line1 = outReader.readLine();
+        String line2 = outReader.readLine();
+        String line3 = outReader.readLine();
+
+        Assert.assertEquals("Derp\tDerp3\tDerp5",line1);
+        Assert.assertEquals("Derp2\tDerp4\tDerp6",line2);
+        Assert.assertEquals("Derp7",line3);
+
+        t1.delete();
+        t2.delete();
+        t3.delete();
+        out.delete();
+    }
+
+}
