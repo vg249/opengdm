478381e7d4ba31cb878690677ddf302bdcf102da
Rodolfo N. Duldulao, Jr <rnduldulaojr@gmail.com>
2020-11-19 02:22:33 +0800

    Added fullName JsonProperty to ContactDTO

--- gobii-model/src/main/java/org/gobiiproject/gobiimodel/dto/gdmv3/ContactDTO.java
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/dto/gdmv3/ContactDTO.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
@@ -76,6 +77,12 @@ public class ContactDTO extends DTOBaseAuditable {
 
     @Getter(AccessLevel.NONE)
     private String piContactId;
+
+    @JsonProperty
+    public String getFullName() {
+        return String.format("%s, %s", this.getPiContactLastName(), this.getPiContactFirstName());
+    }
+
 
     public String getPiContactId() {
         return Optional.ofNullable(contactId)
