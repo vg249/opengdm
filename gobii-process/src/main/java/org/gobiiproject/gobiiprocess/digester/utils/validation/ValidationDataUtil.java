a14ea7e83c4036c70e87b0da31f721feef408900
vg249 <vg249@cornell.edu>
2020-11-19 23:14:45 -0500

    modified getNamesByNameList method to use new mapNamesToIds method

--- gobii-process/src/main/java/org/gobiiproject/gobiiprocess/digester/utils/validation/ValidationDataUtil.java
+++ gobii-process/src/main/java/org/gobiiproject/gobiiprocess/digester/utils/validation/ValidationDataUtil.java
@@ -18,18 +18,12 @@ import org.gobiiproject.gobiimodel.dto.auditable.MapsetDTO;
 import org.gobiiproject.gobiimodel.dto.auditable.PlatformDTO;
 import org.gobiiproject.gobiimodel.dto.auditable.ProjectDTO;
 import org.gobiiproject.gobiimodel.dto.children.NameIdDTO;
-import org.gobiiproject.gobiimodel.entity.Experiment;
-import org.gobiiproject.gobiimodel.entity.Mapset;
-import org.gobiiproject.gobiimodel.entity.Platform;
-import org.gobiiproject.gobiimodel.entity.Project;
+import org.gobiiproject.gobiimodel.entity.*;
 import org.gobiiproject.gobiimodel.types.*;
 import org.gobiiproject.gobiimodel.utils.error.Logger;
 import org.gobiiproject.gobiiprocess.digester.utils.validation.errorMessage.Failure;
 import org.gobiiproject.gobiiprocess.digester.utils.validation.errorMessage.FailureTypes;
-import org.gobiiproject.gobiisampletrackingdao.ExperimentDao;
-import org.gobiiproject.gobiisampletrackingdao.MapsetDao;
-import org.gobiiproject.gobiisampletrackingdao.PlatformDao;
-import org.gobiiproject.gobiisampletrackingdao.ProjectDao;
+import org.gobiiproject.gobiisampletrackingdao.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -44,16 +38,10 @@ public class ValidationDataUtil {
 
     private final static int pageSize = 1000;
 
+    static final int MAX_ENTITIES_PER_QUERY = 2000;
+
     private static ApplicationContext context = new ClassPathXmlApplicationContext(
         "classpath:/spring/application-config.xml");
-
-    private static MapsetDao mapsetDao = context.getBean(MapsetDao.class);
-
-    private static ExperimentDao experimentDao = context.getBean(ExperimentDao.class);
-
-    private static ProjectDao projectDao = context.getBean(ProjectDao.class);
-
-    private static PlatformDao platformDao = context.getBean(PlatformDao.class);
 
     /**
      * Gets the allowed values for foreign key
@@ -66,6 +54,7 @@ public class ValidationDataUtil {
             int offset = 0;
             if (foreignKey.equalsIgnoreCase(ValidationConstants.LINKAGE_GROUP)) {
                 List<Mapset> mapsets = new ArrayList<>();
+                MapsetDao mapsetDao = context.getBean(MapsetDao.class);
                 while (mapsets.size() == pageSize || offset == 0) {
                     mapsets = mapsetDao.getMapsets(pageSize, offset, null);
                     mapsets.forEach(mapset -> {
@@ -77,6 +66,7 @@ public class ValidationDataUtil {
                 }
             } else if (foreignKey.equalsIgnoreCase(ValidationConstants.DNARUN)) {
                 List<Experiment> experiments = new ArrayList<>();
+                ExperimentDao experimentDao = context.getBean(ExperimentDao.class);
                 while (experiments.size() == pageSize || offset == 0) {
                     experiments = experimentDao.getExperiments(pageSize, 0, null);
                     experiments.forEach(experiment -> {
@@ -88,6 +78,7 @@ public class ValidationDataUtil {
                 }
             } else if (foreignKey.equalsIgnoreCase(ValidationConstants.DNASAMPLE_NAME)) {
                 List<Project> projects = new ArrayList<>();
+                ProjectDao projectDao = context.getBean(ProjectDao.class);
                 while (projects.size() == pageSize || offset == 0) {
                     projects = projectDao.getProjects(offset/pageSize, pageSize, null);
                     projects.forEach(project -> {
@@ -121,6 +112,7 @@ public class ValidationDataUtil {
         Map<String, String> platformIdNameMap = new HashMap<>();
         try {
             Integer intPlatformId = Integer.parseInt(platformId);
+            PlatformDao platformDao = context.getBean(PlatformDao.class);
             Platform platform = platformDao.getPlatform(intPlatformId);
             if(platform != null) {
                 platformIdNameMap.put(platform.getPlatformId().toString(),
@@ -150,6 +142,7 @@ public class ValidationDataUtil {
         Map<String, String> projectIdNameMap = new HashMap<>();
         try {
             Integer intProjectId = Integer.parseInt(projectId);
+            ProjectDao projectDao = context.getBean(ProjectDao.class);
             Project project = projectDao.getProject(intProjectId);
             if(project != null) {
                 projectIdNameMap.put(project.getProjectId().toString(),
@@ -179,6 +172,7 @@ public class ValidationDataUtil {
         Map<String, String> experimentIdNameMap = new HashMap<>();
         try {
             Integer intExperimentId = Integer.parseInt(experimentId);
+            ExperimentDao experimentDao = context.getBean(ExperimentDao.class);
             Experiment experiment = experimentDao.getExperiment(intExperimentId);
             if(experiment != null) {
                 experimentIdNameMap.put(
@@ -229,110 +223,46 @@ public class ValidationDataUtil {
         return mapsetIdNameMap;
     }
 
-    //TODO - this limit can be divined by another webservice call
-    static final int DEFAULT_MAX_NAMES_PER_CALL = 2000;
 
-    /**
-     * Web service call to validate CV and reference type. Will batch over the arbitrary limit.
-     *
-     * @param nameIdDTOList       Items list
-     * @param gobiiEntityNameType CV or Reference
-     * @param filterValue         filter value
-     * @param failureList         failure list
-     * @return Items list with id
-     * @throws MaximumErrorsValidationException exception
-     */
-    public static List<NameIdDTO> getNamesByNameList(List<NameIdDTO> nameIdDTOList, String gobiiEntityNameType, String filterValue, List<Failure> failureList, GobiiCropConfig cropConfig) throws MaximumErrorsValidationException {
-        int numEntities = nameIdDTOList.size();
+    public static List<NameIdDTO> getNamesByNameList(List<String> names,
+                                                     String gobiiEntityNameType,
+                                                     String filterValue,
+                                                     List<Failure> failureList
+    ) throws GobiiDaoException {
+
+        int numEntities = names.size();
+
         List<NameIdDTO> results = new ArrayList<>(numEntities);
-        int maxEntitiesPerCall = DEFAULT_MAX_NAMES_PER_CALL;
+        int maxEntitiesPerCall = MAX_ENTITIES_PER_QUERY;
 
-        //Determine limit from configuration if possible
-        Integer limit = cropConfig != null ? getEntityLimit(gobiiEntityNameType,cropConfig):null;
-        if((limit!=null) && (limit > 0)){
-            maxEntitiesPerCall = limit;
-        }
+        for(int i=0; i < numEntities; i+=maxEntitiesPerCall) {
 
-        for(int i=0;i < numEntities;i+=maxEntitiesPerCall){
-            List<NameIdDTO> sublist = getSubList(nameIdDTOList,i,i+maxEntitiesPerCall);
-            results.addAll(getNamesByShortNameList(sublist,gobiiEntityNameType,filterValue,failureList));
+            Set<String> namesSubSet = new HashSet<>(names.subList(i, i+maxEntitiesPerCall));
+
+            results.addAll(mapNamesToIds(namesSubSet,
+                gobiiEntityNameType,
+                filterValue,
+                failureList));
         }
         return results;
     }
 
-    private static Integer getEntityLimit(String gobiiEntityNameType, GobiiCropConfig cropConfig) {
-        Integer limit = null;
+    private static List<NameIdDTO> mapNamesToIds(Set<String> names,
+                                                 String gobiiEntityNameType,
+                                                 String filterValue,
+                                                 List<Failure> failureList
+    ) throws MaximumErrorsValidationException {
+        List<NameIdDTO> nameIdDTOs = new ArrayList<>();
+
         try {
-            limit = cropConfig.getServer(ServerType.GOBII_WEB).getRestResourceLimit(RestResourceId.GOBII_NAMES,RestMethodType.GET,gobiiEntityNameType);
-        }catch(Exception e){
-            //Program flow - getRestResourceLimit follows success or exception paradigm
-        }
-        if(limit != null){
-            return limit;
-        }
-        try{
-            limit = cropConfig.getServer(ServerType.GOBII_WEB).getRestResourceLimit(RestResourceId.GOBII_NAMES,RestMethodType.GET,gobiiEntityNameType);
-        }catch(Exception e){
-            //Program flow - getRestResourceLimit follows success or exception paradigm
-        }
-        return limit;
-    }
+            switch (gobiiEntityNameType.toLowerCase()) {
 
-    /**
-     * Works like ArrayList.getSubList, but if last argument is longer than the list length, truncates instead of warning.
-     * Works on both ArrayLists and LinkedLists, but only ArrayList implementation is efficient, so generates a warning
-     * on non-Array lists. Analysis of existing codebase suggests primarily Array Lists are being used.
-     * @param inList input list - preferably an ArrayList, but will work with other list types.
-     * @param fromList index 'from' to begin sublist at
-     * @param toList index 'to' to end sublist at. Sublist will end early if 'to' is too large
-     * @param <T>
-     * @return
-     */
-    private static<T> List<T> getSubList(List<T> inList, int fromList, int toList){
-        if(toList > inList.size()){
-            toList = inList.size();
-        }
-        if(inList instanceof ArrayList){
-            ArrayList<T> aList = (ArrayList<T>)inList;
-            return aList.subList(fromList,toList);
-        }
+                case GobiiEntityNameType.DNARUN.name().toLowerCase():
+                    nameIdDTOs = getAnalysesNameIds(names, );
+                    break;
 
-        Logger.logWarning("Validation", "Inefficient call to getSubList on non-array list type");
-        List<T> outList = new LinkedList<T>();
-        Iterator<T> interator = inList.listIterator(fromList);
-        for(int i = 0; i < toList - fromList; i++){
-            if(!interator.hasNext()){
-                break;
             }
-            outList.add(interator.next()); //bleugh
-        }
-        return outList;
-    }
 
-    /**
-     * Underlying web service call to validate CV and reference type
-     *
-     * @param nameIdDTOList       Items list
-     * @param gobiiEntityNameType CV or Reference
-     * @param filterValue         filter value
-     * @param failureList         failure list
-     * @return Items list with id
-     * @throws MaximumErrorsValidationException exception
-     */
-    public static List<NameIdDTO> getNamesByShortNameList(List<NameIdDTO> nameIdDTOList, String gobiiEntityNameType, String filterValue, List<Failure> failureList) throws MaximumErrorsValidationException {
-        List<NameIdDTO> nameIdDTOListResponse = new ArrayList<>();
-        try {
-            PayloadEnvelope<NameIdDTO> payloadEnvelope = new PayloadEnvelope<>();
-            payloadEnvelope.getHeader().setGobiiProcessType(GobiiProcessType.CREATE);
-            payloadEnvelope.getPayload().setData(nameIdDTOList);
-
-            RestUri namesUri = GobiiClientContext.getInstance(null, false)
-                    .getUriFactory()
-                    .nameIdListByQueryParams();
-            GobiiEnvelopeRestResource<NameIdDTO, NameIdDTO> gobiiEnvelopeRestResource = new GobiiEnvelopeRestResource<>(namesUri);
-            namesUri.setParamValue("entity", gobiiEntityNameType.toLowerCase());
-
-            namesUri.setParamValue("filterType", StringUtils.capitalize(GobiiFilterType.NAMES_BY_NAME_LIST.toString().toUpperCase()));
             if (gobiiEntityNameType.equalsIgnoreCase(GobiiEntityNameType.CV.toString())) {
                 switch (filterValue) {
                     case "species_name":
@@ -350,24 +280,25 @@ public class ValidationDataUtil {
                 }
             } else namesUri.setParamValue("filterValue", filterValue);
 
-            PayloadEnvelope<NameIdDTO> responsePayloadEnvelope = gobiiEnvelopeRestResource.post(NameIdDTO.class, payloadEnvelope);
-            Status status = responsePayloadEnvelope.getHeader().getStatus();
-            if (!status.isSucceeded()) {
-                Logger.logWarning("ValidtionWebServices","Bad NameIdDTO request");
-                for (HeaderStatusMessage m : status.getStatusMessages()) {
-                    Logger.logWarning("ValidationWebServices", m.getMessage());
-                }
-                ArrayList<HeaderStatusMessage> statusMessages = status.getStatusMessages();
-                for (HeaderStatusMessage message : statusMessages)
-                    ValidationUtil.createFailure(FailureTypes.DATABASE_ERROR, new ArrayList<>(), message.getMessage(), failureList);
-                return nameIdDTOListResponse;
-            }
-            nameIdDTOListResponse.addAll(responsePayloadEnvelope.getPayload().getData());
         } catch (MaximumErrorsValidationException e) {
             throw e;
         } catch (Exception e) {
             ValidationUtil.createFailure(FailureTypes.EXCEPTION, new ArrayList<>(), e.getMessage(), failureList);
         }
-        return nameIdDTOListResponse;
+        return nameIdDTOs;
+    }
+
+    private static List<NameIdDTO> getAnalysesNameIds(Set<String> names) {
+        List<NameIdDTO> nameIdDTOs = new ArrayList<>();
+        AnalysisDao analysisDao = context.getBean(AnalysisDao.class);
+        List<Analysis> analyses = analysisDao.getAnalysesByAnalysisNames(names);
+        for(Analysis analysis : analyses) {
+            nameIdDTOs.add(new NameIdDTO(analysis.getAnalysisId(), analysis.getAnalysisName()));
+        }
+        return nameIdDTOs;
+    }
+
+    private static NameIdDTO getNameIdDTO(Integer id, String name) {
+        return
     }
 }
