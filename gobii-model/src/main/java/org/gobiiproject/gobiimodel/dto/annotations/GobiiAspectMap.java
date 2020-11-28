24c150da42e44d36db6e5df915caf29834a8310c
vg249 <vg249@cornell.edu>
2020-11-28 01:35:01 -0500

    new annotation to map loader template directly to aspect tables

--- gobii-model/src/main/java/org/gobiiproject/gobiimodel/dto/annotations/GobiiAspectMap.java
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/dto/annotations/GobiiAspectMap.java
@@ -1,4 +1,27 @@
 package org.gobiiproject.gobiimodel.dto.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * To map template field to Aspect fields
+ * <p>
+ * To map dto field to aspect field. Will be ignored when defined alone.
+ * Had to be defined as array value in {@link GobiiAspectMaps}
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.ANNOTATION_TYPE)
 public @interface GobiiAspectMap {
+
+    /**
+     * Name of aspect field to which the value needs to be mapped
+     */
+    String paramName() default "";
+
+    /**
+     * Aspect table to which the field should be mapped.
+     */
+    Class<?> aspectTable() default void.class;
 }
