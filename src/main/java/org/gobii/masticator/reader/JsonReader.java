c97667006b97417dc2b65d523e30a3f41590edca
vg249 <vg249@cornell.edu>
2020-11-30 13:57:21 -0500

    Squashed 'gobii-masticator/' changes from 66ffb1db0..cc954596f
    cc954596f Merged in single-quotes-around-json-type (pull request #4)
    3e92722fd Adding single quotes around both elements to hopefully fix issue with IFL read.
    
    git-subtree-dir: gobii-masticator
    git-subtree-split: cc954596f282fdaff0673cba9aa2a04988a4ffb3

--- src/main/java/org/gobii/masticator/reader/JsonReader.java
+++ src/main/java/org/gobii/masticator/reader/JsonReader.java
@@ -21,7 +21,7 @@ public class JsonReader implements Reader {
 					String[] toks = s.split("\t", -1);
 					StringJoiner joiner = new StringJoiner(",");
 					for (int i = 0; i < toks.length; i++) {
-						joiner.add(String.format("%s:%s", reader.getHeader().get(i), toks[i]));
+						joiner.add(String.format("'%s':'%s'", reader.getHeader().get(i), toks[i]));
 					}
 
 					return String.format("\"{%s}\"", joiner.toString());
