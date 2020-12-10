fbc351b84417e3a35820fcca045295443ce17ead
vg249 <vg249@cornell.edu>
2020-12-09 19:28:43 -0500

    modified @DatasetDao and @VariaServiceImpl to use new dataset_stats for get marker and dnarun counts

--- gobii-domain/src/main/java/org/gobiiproject/gobiidomain/services/brapi/VariantSetsServiceImpl.java
+++ gobii-domain/src/main/java/org/gobiiproject/gobiidomain/services/brapi/VariantSetsServiceImpl.java
@@ -82,8 +82,6 @@ public class VariantSetsServiceImpl implements VariantSetsService {
 
                 Dataset dataset = (Dataset) tuple[0];
                 Analysis analysis = (Analysis) tuple[1];
-                Integer markerCount = (Integer) tuple[2];
-                Integer dnaRunCount = (Integer) tuple[3];
 
                 if(!variantSetDtoMapByDatasetId.containsKey(dataset.getDatasetId())) {
 
@@ -104,9 +102,6 @@ public class VariantSetsServiceImpl implements VariantSetsService {
                         MessageFormat.format(this.fileUrlFormat, dataset.getDatasetId()));
                     variantSetDTO.getAvailableFormats().add(fileFormat);
 
-                    //Set Marker and DnaRun Counts
-                    variantSetDTO.setVariantCount(markerCount);
-                    variantSetDTO.setCallSetCount(dnaRunCount);
 
                     //Map extract ready of dataset
                     mapVariantSetExtractReady(dataset, variantSetDTO);
