e68a66dc8f8a8027ea441227d08634f08888ac8b
vg249 <vg249@cornell.edu>
2021-02-02 23:52:14 -0500

    added a Builder for DigesterResult as large number of fields are initialized

--- gobii-model/src/main/java/org/gobiiproject/gobiimodel/dto/instructions/loader/DigesterResult.java
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/dto/instructions/loader/DigesterResult.java
@@ -1,6 +1,5 @@
 package org.gobiiproject.gobiimodel.dto.instructions.loader;
 
-import com.google.gson.internal.$Gson$Preconditions;
 import org.gobiiproject.gobiimodel.config.GobiiCropConfig;
 
 import java.io.File;
@@ -35,31 +34,118 @@ public class DigesterResult {
 
     private String contactEmail;
 
-    public DigesterResult(boolean success,
-                          boolean sendQc,
-                          String cropType,
-                          GobiiCropConfig cropConfig,
-                          String intermediateFilePath,
-                          String loadType,
-                          Map<String, File> loaderInstructionsMap,
-                          List<String> loaderInstructionsList,
-                          String datasetType,
-                          Object jobStatusObject,
-                          Integer datasetId,
-                          String jobName) {
+    public DigesterResult(Builder builder) {
+        this.success = builder.success;
+        this.sendQc = builder.sendQc;
+        this.cropType = builder.cropType;
+        this.cropConfig = builder.cropConfig;
+        this.intermediateFilePath = builder.intermediateFilePath;
+        this.loadType = builder.loadType;
+        this.loaderInstructionsMap = builder.loaderInstructionsMap;
+        this.loaderInstructionsList = builder.loaderInstructionsList;
+        this.datasetType = builder.datasetType;
+        this.jobStatusObject = builder.jobStatusObject;
+        this.datasetId = builder.datasetId;
+        this.jobName = builder.jobName;
+        this.contactEmail = builder.contactEmail;
+    }
 
-        this.success = success;
-        this.sendQc = sendQc;
-        this.cropType = cropType;
-        this.cropConfig = cropConfig;
-        this.intermediateFilePath = intermediateFilePath;
-        this.loadType = loadType;
-        this.loaderInstructionsMap = loaderInstructionsMap;
-        this.loaderInstructionsList = loaderInstructionsList;
-        this.datasetType = datasetType;
-        this.jobStatusObject = jobStatusObject;
-        this.datasetId = datasetId;
-        this.jobName = jobName;
+    public static class Builder {
+        private boolean success;
+
+        private boolean sendQc;
+
+        private String cropType;
+
+        private GobiiCropConfig cropConfig;
+
+        private String intermediateFilePath;
+
+        private String loadType;
+
+        private Map<String, File> loaderInstructionsMap;
+
+        private List<String> loaderInstructionsList;
+
+        private String datasetType;
+
+        private Object jobStatusObject;
+
+        private Integer datasetId;
+
+        private String jobName;
+
+        private String contactEmail;
+
+        public Builder setSuccess(boolean success) {
+            this.success = success;
+            return this;
+        }
+        
+        public Builder setSendQc(boolean sendQc) {
+            this.sendQc = sendQc;
+            return this;
+        }
+
+        public Builder setIntermediateFilePath(String intermediateFilePath) {
+            this.intermediateFilePath = intermediateFilePath;
+            return this;
+        }
+
+        public Builder setCropType(String cropType) {
+            this.cropType = cropType;
+            return this;
+        }
+
+        public Builder setCropConfig(GobiiCropConfig cropConfig) {
+            this.cropConfig = cropConfig;
+            return this;
+        }
+
+        public Builder setLoadType(String loadType) {
+            this.loadType = loadType;
+            return this;
+        }
+
+        public Builder setLoaderInstructionsMap(Map<String, File> loaderInstructionsMap) {
+            this.loaderInstructionsMap = loaderInstructionsMap;
+            return this;
+        }
+
+        public Builder setLoaderInstructionsList(List<String> loaderInstructionsList) {
+            this.loaderInstructionsList = loaderInstructionsList;
+            return this;
+        }
+        
+        public Builder setDatasetType(String datasetType) {
+            this.datasetType = datasetType;
+            return this;
+        }
+
+        public Builder setJobStatusObject(Object jobStatusObject) {
+            this.jobStatusObject = jobStatusObject;
+            return this;
+        }
+        
+        public Builder setDatasetId(Integer datasetId) {
+            this.datasetId = datasetId;
+            return this;
+        }
+
+        public Builder setJobName(String jobName) {
+            this.jobName = jobName;
+            return this;
+        }
+        
+        public Builder setContactEmail(String contactEmail) {
+            this.contactEmail = contactEmail;
+            return this;
+        }
+
+        public DigesterResult build() {
+            return new DigesterResult(this);
+        }
+
     }
 
     public boolean isSuccess() {
