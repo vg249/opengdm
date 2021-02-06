e89abb31f51c6f0065acc2d6041a1e4848f29812
vg249 <vg249@cornell.edu>
2021-02-05 23:39:00 -0500

    moved code blocks creating each Aspect table to separate private methods
    added MatrixTranformTable aspect processing

--- gobii-process/src/main/java/org/gobiiproject/gobiiprocess/digester/aspectsdigest/HapmapsDigest.java
+++ gobii-process/src/main/java/org/gobiiproject/gobiiprocess/digester/aspectsdigest/HapmapsDigest.java
@@ -9,14 +9,19 @@ import java.util.Map;
 import java.util.Scanner;
 
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.docx4j.model.datastorage.XPathEnhancerParser.primaryExpr_return;
 import org.gobiiproject.gobiimodel.config.ConfigSettings;
 import org.gobiiproject.gobiimodel.config.GobiiException;
+import org.gobiiproject.gobiimodel.cvnames.DatasetType;
 import org.gobiiproject.gobiimodel.dto.gdmv3.GenotypeUploadRequestDTO;
 import org.gobiiproject.gobiimodel.dto.instructions.loader.DigesterResult;
+import org.gobiiproject.gobiimodel.dto.instructions.loader.MasticatorResult;
 import org.gobiiproject.gobiimodel.dto.instructions.loader.v3.ColumnAspect;
 import org.gobiiproject.gobiimodel.dto.instructions.loader.v3.DatasetDnaRunTable;
 import org.gobiiproject.gobiimodel.dto.instructions.loader.v3.DatasetMarkerTable;
 import org.gobiiproject.gobiimodel.dto.instructions.loader.v3.LoaderInstruction;
+import org.gobiiproject.gobiimodel.dto.instructions.loader.v3.MarkerTable;
 import org.gobiiproject.gobiimodel.dto.instructions.loader.v3.MatrixAspect;
 import org.gobiiproject.gobiimodel.dto.instructions.loader.v3.MatrixTable;
 import org.gobiiproject.gobiimodel.dto.instructions.loader.v3.MatrixTransformAspect;
@@ -24,8 +29,12 @@ import org.gobiiproject.gobiimodel.dto.instructions.loader.v3.MatrixTransformTab
 import org.gobiiproject.gobiimodel.dto.instructions.loader.v3.RangeAspect;
 import org.gobiiproject.gobiimodel.dto.instructions.loader.v3.RowAspect;
 import org.gobiiproject.gobiimodel.dto.instructions.loader.v3.Table;
+import org.gobiiproject.gobiimodel.entity.Dataset;
 import org.gobiiproject.gobiimodel.utils.AspectUtils;
 import org.gobiiproject.gobiiprocess.digester.utils.GobiiFileUtils;
+import org.gobiiproject.gobiiprocess.spring.SpringContextLoaderSingleton;
+import org.gobiiproject.gobiisampletrackingdao.DatasetDao;
+import org.gobiiproject.gobiisampletrackingdao.DatasetDaoImpl;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -57,9 +66,14 @@ public class HapmapsDigest extends AspectDigest {
                "markerPositionStart", "pos",
                "markerPositionStop", "pos");
 
+    private GenotypeUploadRequestDTO uploadRequest;
+
+    private DatasetDao datasetDao;
+
     HapmapsDigest(LoaderInstruction loaderInstruction, 
                   ConfigSettings configSettings) throws GobiiException {
         super(loaderInstruction, configSettings);
+        this.datasetDao = SpringContextLoaderSingleton.getInstance().getBean(DatasetDao.class);
     }
 
     private boolean hasValidHapmaHeaders(String[] headers) {
@@ -75,7 +89,13 @@ public class HapmapsDigest extends AspectDigest {
     
         // creates new directtory or cleans one if already exists
         setupOutputDirectory();            
-    
+                
+        uploadRequest = mapper.convertValue(
+            loaderInstruction.getUserRequest(), 
+            GenotypeUploadRequestDTO.class);                    
+                
+        Map<String, MasticatorResult> masticatedFilesMap = new HashMap<>();
+
         try {
 
             filesToDigest = getFilesToDigest();
@@ -118,72 +138,41 @@ public class HapmapsDigest extends AspectDigest {
                 if(!hasValidHapmaHeaders(headerColumns)) {
 
                 }
-                GenotypeUploadRequestDTO uploadRequest = mapper.convertValue(
-                    loaderInstruction.getUserRequest(), 
-                    GenotypeUploadRequestDTO.class);                    
-                //
-                // Create dataset marker aspects                
-                //
-                DatasetMarkerTable datasetMarkerTable = new DatasetMarkerTable();
+                
+                // Set dataset_marker table aspect               
                 String datasetMarkerTableName = AspectUtils.getTableName(DatasetMarkerTable.class);
+                int hdf5Start = 1;
+                if(masticatedFilesMap.containsKey(datasetMarkerTableName)) {
+                    hdf5Start = 
+                        masticatedFilesMap.get(datasetMarkerTableName).getTotalLinesWritten();
+                }
+                aspects.put(datasetMarkerTableName, 
+                            getDatasetMarkerTable(headerColumns, headerLineNumberIndex, hdf5Start));
 
-                datasetMarkerTable.setDatasetId(uploadRequest.getDatasetId());
-                datasetMarkerTable.setPlatformId(uploadRequest.getPlatformId());
-
-                // Set column aspect for marker name.
-                int markerNameColumnIndex = 
-                    ArrayUtils.indexOf(headerColumns, 
-                                       aspectTableColumnsToHapmapColumns.get("markerName"));
-                ColumnAspect markerNameColumn = 
-                    new ColumnAspect(headerLineNumberIndex+1, markerNameColumnIndex);
-                datasetMarkerTable.setMarkerName(markerNameColumn);
-
-                // Set range aspect for hdf5 index.
-                datasetMarkerTable.setDatasetMarkerIdx(new RangeAspect(1));
-
-                //
                 // Create dataset danrun aspects
-                //
-                DatasetDnaRunTable datasetDnaRunTable = new DatasetDnaRunTable();
                 String datasetDnaRunTableName = AspectUtils.getTableName(DatasetDnaRunTable.class);
+                aspects.put(datasetDnaRunTableName, 
+                            getDatasetDnaRunTable(headerLineNumberIndex, hdf5Start));
 
-                datasetDnaRunTable.setDatasetId(uploadRequest.getDatasetId());
-                datasetDnaRunTable.setPlatformId(uploadRequest.getPlatformId());
-
-                // Dnarun names starts after required columns in the file
-                RowAspect dnaRunNameRow = new RowAspect(headerLineNumberIndex, 
-                                                        hapMapRequiredColumns.length);
-                datasetDnaRunTable.setDnaRunName(dnaRunNameRow);
+                if(StringUtils.isNotEmpty(uploadRequest.getDataType()) &&
+                    dataTypeToTransformType.containsKey(uploadRequest.getDataType())) {
+                    String matrixTableName = AspectUtils.getTableName(MatrixTransformTable.class);
+                    aspects.put(matrixTableName, 
+                                getMatrixTransformTable(headerLineNumberIndex, 
+                                                        uploadRequest.getDataType()));
+                }
+                else {
+                    String matrixTableName = AspectUtils.getTableName(MatrixTable.class);
+                    aspects.put(matrixTableName, getMatrixTable(headerLineNumberIndex));
+                }
                 
-                // Set Range aspect for hdf5 index                   
-                datasetDnaRunTable.setDatasetDnaRunIdx(new RangeAspect(1)); 
-
-               
-                //MatrixTable matrixTable = new MatrixTable();
-                //String matrixTableName = AspectUtils.getTableName(MatrixTable.class);
-                //MatrixAspect matrixAspect = new MatrixAspect(headerLineNumberIndex+1, 
-                //                                             hapMapRequiredColumns.length);
-                //matrixTable.setMatrix(matrixAspect);
-                
-                MatrixTransformTable matrixTable = new MatrixTransformTable();
-                String matrixTableName = AspectUtils.getTableName(MatrixTransformTable.class);
-                MatrixTransformAspect matrixAspect = new MatrixTransformAspect(
-                    "TWOLETTERNUCLEOTIDE",
-                    headerLineNumberIndex+1, 
-                    hapMapRequiredColumns.length);
-                    
-                matrixTable.setMatrix(matrixAspect);
-                
-                aspects.put(datasetMarkerTableName, datasetMarkerTable);
-                aspects.put(datasetDnaRunTableName, datasetDnaRunTable);
-                aspects.put(matrixTableName, matrixTable);
 
                 // Masticate and set the output.
-                Map<String, File> masticatedFilesMap = masticate(aspects);
+                masticatedFilesMap = masticate(aspects);
 
                 // Update the intermediate file map incase if there is any new table
-                masticatedFilesMap.forEach((table, filePath) -> {
-                    intermediateDigestFileMap.put(table, filePath);
+                masticatedFilesMap.forEach((table, masticatorResult) -> {
+                    intermediateDigestFileMap.put(table, masticatorResult.getOutputFile());
                 });
 
             }
@@ -192,9 +181,107 @@ public class HapmapsDigest extends AspectDigest {
             log.error(e.getMessage());
             throw new GobiiException(e);
         }
+        
+        // Get the load order from ifl config
+        List<String> loadOrder = getLoadOrder();
+        if(loadOrder == null || loadOrder.size() <= 0) {
+            loadOrder = new ArrayList<>(intermediateDigestFileMap.keySet());
+        }
 
-        return null;
+        DigesterResult digesterResult = new DigesterResult
+                .Builder()
+                .setSuccess(true)
+                .setSendQc(false)
+                .setCropType(loaderInstruction.getCropType())
+                .setCropConfig(cropConfig)
+                .setIntermediateFilePath(loaderInstruction.getOutputDir())
+                .setLoadType(loaderInstruction.getLoadType())
+                .setLoaderInstructionsMap(intermediateDigestFileMap)
+                .setLoaderInstructionsList(loadOrder)
+                .setDatasetType(uploadRequest.getDataType())
+                .setJobStatusObject(jobStatus)
+                .setDatasetId(null)
+                .setJobName(loaderInstruction.getJobName())
+                .setContactEmail(loaderInstruction.getContactEmail())
+                .build();
+
+        return digesterResult;
     }
 
-    
+    private DatasetMarkerTable getDatasetMarkerTable(
+        String[] headers, 
+        int headerLineNumber,
+        int hdf5Start) {
+
+        DatasetMarkerTable datasetMarkerTable = new DatasetMarkerTable();
+
+        datasetMarkerTable.setDatasetId(uploadRequest.getDatasetId());
+        datasetMarkerTable.setPlatformId(uploadRequest.getPlatformId());
+
+        // Set column aspect for marker name.
+        int markerNameColumnIndex = 
+            ArrayUtils.indexOf(headers, 
+                               aspectTableColumnsToHapmapColumns.get("markerName"));
+        // data lines starts after header line
+        ColumnAspect markerNameColumn = 
+            new ColumnAspect(headerLineNumber+1, markerNameColumnIndex);
+        datasetMarkerTable.setMarkerName(markerNameColumn);
+        
+        // Set range aspect for hdf5 index.
+        datasetMarkerTable.setDatasetMarkerIdx(new RangeAspect(hdf5Start));
+        
+        return datasetMarkerTable;
+    }
+
+    private DatasetDnaRunTable getDatasetDnaRunTable(int headerLineNumber,
+                                                     int hdf5Start) throws GobiiException {
+        
+       DatasetDnaRunTable datasetDnaRunTable = new DatasetDnaRunTable();
+        
+       datasetDnaRunTable.setDatasetId(uploadRequest.getDatasetId());
+       datasetDnaRunTable.setPlatformId(uploadRequest.getPlatformId());
+
+       // Get Experiment id
+       Integer datasetId;
+       try {
+           datasetId = Integer.parseInt(uploadRequest.getDatasetId());
+       }
+       catch(NumberFormatException e) {
+           throw new GobiiException("Invalid dataset id");
+       }
+       Dataset dataset = datasetDao.getDataset(datasetId);
+       datasetDnaRunTable.setExperimentId(dataset.getExperiment().getExperimentId().toString());
+
+       // Dnarun names starts after required columns in the file
+       RowAspect dnaRunNameRow = new RowAspect(headerLineNumber, 
+                                               hapMapRequiredColumns.length);
+       datasetDnaRunTable.setDnaRunName(dnaRunNameRow);
+        
+       // Set Range aspect for hdf5 index                   
+       datasetDnaRunTable.setDatasetDnaRunIdx(new RangeAspect(1));
+       
+       return datasetDnaRunTable;
+
+    }
+
+    private MatrixTransformTable getMatrixTransformTable(int headerLineNumber, 
+                                                         String dataType) {
+        MatrixTransformTable matrixTable = new MatrixTransformTable();
+        MatrixTransformAspect matrixAspect = new MatrixTransformAspect(
+            dataTypeToTransformType.get(dataType),
+            headerLineNumber+1, 
+            hapMapRequiredColumns.length);
+        matrixTable.setMatrix(matrixAspect);
+        return matrixTable;
+    }
+
+    private MatrixTable getMatrixTable(int headerLineNumber) {
+        MatrixTable matrixTable = new MatrixTable();
+        MatrixAspect matrixAspect = new MatrixAspect(headerLineNumber+1, 
+                                                     hapMapRequiredColumns.length);
+
+        matrixTable.setMatrix(matrixAspect);
+        return matrixTable;
+    }
+
 }
