fa547eb018d0835ff969f26307a0ae23d83130df
vg249 <vg249@cornell.edu>
2021-04-10 14:38:41 -0400

    fix Intitialized platform_stats mapping

--- gobii-model/src/main/java/org/gobiiproject/gobiimodel/entity/Platform.java
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/entity/Platform.java
@@ -60,7 +60,4 @@ public class Platform extends BaseEntity{
     @JoinColumn(name = "status", referencedColumnName = "cv_id")
     private Cv status;
 
-    @OneToOne(mappedBy = "platform")
-    private PlatformStats platformStats;
-
 }
