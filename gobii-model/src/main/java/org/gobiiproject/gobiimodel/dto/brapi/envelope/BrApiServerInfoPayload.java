bfe0ebd67b26a13352557ae9b6d3b7187ebf924e
vg249 <vg249@cornell.edu>
2021-05-23 20:39:39 -0400

    Added OIDC settings to serverinfo

--- gobii-model/src/main/java/org/gobiiproject/gobiimodel/dto/brapi/envelope/BrApiServerInfoPayload.java
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/dto/brapi/envelope/BrApiServerInfoPayload.java
@@ -2,6 +2,8 @@ package org.gobiiproject.gobiimodel.dto.brapi.envelope;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import org.gobiiproject.gobiimodel.dto.brapi.AccessFlowDTO;
+
 import java.util.List;
 
 @SuppressWarnings("rawtypes")
@@ -11,14 +13,17 @@ public class BrApiServerInfoPayload<ServerInfoDTO>  extends BrApiMasterPayload {
     private BrApiResultServerInfo<ServerInfoDTO> result =
             new BrApiResultServerInfo<>();
 
-    public BrApiServerInfoPayload(List<ServerInfoDTO> listCalls) {
-        this.result.setCalls(listCalls);
+    public BrApiServerInfoPayload(List<ServerInfoDTO> calls, List<AccessFlowDTO> accessFlows) {
+        this.result.setCalls(calls);
+        this.result.setAccessFlows(accessFlows);
     }
 
-    public BrApiServerInfoPayload(List<ServerInfoDTO> listCalls,
+    public BrApiServerInfoPayload(List<ServerInfoDTO> calls,
+                                  List<AccessFlowDTO> accessFlows,
                                   Integer pageSize,
                                   Integer currentPage) {
-        this.result.setCalls(listCalls);
+        this.result.setCalls(calls);
+        this.result.setAccessFlows(accessFlows);
         this.getMetadata().getPagination().setPageSize(pageSize);
         this.getMetadata().getPagination().setCurrentPage(currentPage);
     }
