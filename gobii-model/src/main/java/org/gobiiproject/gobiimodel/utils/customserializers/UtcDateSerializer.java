a57023e9a28a16bb1d485335b8b2f4e2fa0026e6
Vishnu Govindaraj <vg249@cornell.edu>
2021-07-28 11:26:53 -0400

    Fix UTC dateforamt to 0-23 hour format to avoid am/pm conflicts

--- gobii-model/src/main/java/org/gobiiproject/gobiimodel/utils/customserializers/UtcDateSerializer.java
+++ gobii-model/src/main/java/org/gobiiproject/gobiimodel/utils/customserializers/UtcDateSerializer.java
@@ -1,14 +1,14 @@
 package org.gobiiproject.gobiimodel.utils.customserializers;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
 /**
  * Helper class for Jackson Json serializer for Date objects.
@@ -19,7 +19,7 @@ import java.util.TimeZone;
 public class UtcDateSerializer extends StdSerializer<Date> {
 
     private SimpleDateFormat formatter = new SimpleDateFormat(
-            "yyyy-MM-dd'T'hh:mm:ss");
+            "yyyy-MM-dd'T'HH:mm:ss");
 
     public UtcDateSerializer() {
         this(null);
