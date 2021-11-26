3a3431104c03a1d0080a7ef1807dccfd2b3a0b58
err87 <err87@cornell.edu>
2021-11-26 10:49:54 -0500

    Major refactor and simplification.

--- gobii-process/src/test/kotlin/org/gobiiproject/gobiiprocess/translator/utils/TempFiles.kt
+++ gobii-process/src/test/kotlin/org/gobiiproject/gobiiprocess/translator/utils/TempFiles.kt
@@ -1,41 +1,39 @@
 package org.gobiiproject.gobiiprocess.translator.utils
 
 import java.io.*
+import java.nio.file.Path
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 import java.util.stream.Stream
 
-class TempFiles2(verbose: Boolean = false) {
-    val inputFile:             File = createTempFile("input-mf")
-    val lookupFile:            File = createTempFile("lookup")
-    val encodedFile:           File = createTempFile("encoded-mf")
-    val decodedFile:           File = createTempFile("decoded")
-    val sampleFastEncodedFile: File = createTempFile("encoded-sf")
-    val sampleFastInputFile:   File = createTempFile("input-sf")
-    private val timestamp:     String =
-        LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyMMdd-HHmmssnnnnnn"))
-    private val alleles: List<String> by lazy {
-        listOf("A", "C", "G", "T").run {
-            flatMap { i -> flatMap { j -> flatMap { k -> listOf("$i$j$k", "$i$j${k}N") } } }
-        }
-            .dropLast(1)
+class TempFiles {
+    private val timestamp       = LocalDateTime.now()
+        .format(DateTimeFormatter.ofPattern("yyMMdd-HHmmssnnnnnn"))
+        .dropLastWhile { it == '0' }
+    private val tmpDir          = Path.of("src/test/tmp/$timestamp").toFile().also(File::mkdir)
+    val inputFile               = File("$tmpDir/input-mf.txt")
+    val lookupFile              = File("$tmpDir/lookup.txt")
+    val encodedFile             = File("$tmpDir/encoded-mf.txt")
+    val decodedFile             = File("$tmpDir/decoded.txt")
+    val sampleFastEncodedFile   = File("$tmpDir/encoded-sf.txt")
+    val sampleFastInputFile     = File("$tmpDir/input-sf.txt")
+
+    private val random = Random()
+
+    override fun toString(): String {
+        return """
+        ${tmpDir.absolutePath}/
+            ${inputFile.name}
+            ${sampleFastInputFile.name}
+            ${lookupFile.name}
+            ${encodedFile.name}
+            ${sampleFastEncodedFile.name}
+            ${decodedFile.name}
+        """.trimIndent()
     }
 
-    init {
-        if (verbose) {
-            System.err.println(
-                """
-                Input file: $inputFile
-                Input file (sf): $sampleFastInputFile
-                Lookup file: $lookupFile
-                Encoded file: $encodedFile
-                Encoded file (sf): $sampleFastEncodedFile
-                Decoded file: $decodedFile
-                """.trimIndent()
-            )
-        }
-    }
+    fun cleanup() = tmpDir.deleteRecursively()
 
     fun worstCaseInputFile(
         numMarkers: Int = 40_000,
@@ -44,7 +42,7 @@ class TempFiles2(verbose: Boolean = false) {
         alleleSeparator: String = "/",
         genotypeSeparator: String = "\t"
     ) {
-        val genotypes = alleles.windowed(ploidy) { it.joinToString(alleleSeparator) }
+        val genotypes = Companion.alleles.windowed(ploidy) { it.joinToString(alleleSeparator) }
         val colReps = numSamples / genotypes.size + 1
         inputFile.bufferedWriter().use { writer ->
             val genotypeString = genotypes
@@ -58,36 +56,6 @@ class TempFiles2(verbose: Boolean = false) {
                 .joinToString("\n") { "$it$genotypeSeparator".repeat(numMarkers).dropLast(1) }
             repeat(colReps) { writer.appendLine(genotypeString) }
         }
-    }
-
-    private fun createTempFile(prefix: String): File {
-        val tmpDir = System.getProperty("java.io.tmpdir")
-        val path = tmpDir + "/" + timestamp + "_" + prefix + ".tmp"
-        return File(path)
-    }
-
-    fun createLargeInputFile(rows: Int, cols: Int) {
-        val writer = BufferedWriter(FileWriter(inputFile))
-        var j = 0
-        var i = 0
-        var sameLine: String? = null
-        while (i <= rows) {
-            if (sameLine == null) {
-                val lineJoiner = StringJoiner("\t")
-                while (j <= cols) {
-                    lineJoiner.add("A/A")
-                    lineJoiner.add("ACGT/ACGT")
-                    lineJoiner.add("ACGTA/ACG")
-                    lineJoiner.add("TTTT/T")
-                    j += 4
-                }
-                sameLine = lineJoiner.toString()
-            }
-            writer.append(sameLine)
-            writer.append(System.lineSeparator())
-            i++
-        }
-        writer.close()
     }
 
     fun createRandomInputFile(rows: Int, cols: Int) {
@@ -117,7 +85,18 @@ class TempFiles2(verbose: Boolean = false) {
         writer.close()
     }
 
-    fun getTransposedFileStream(file: File?): Stream<String> {
+    fun createSimpleRandomInputFile(rows: Int, cols: Int, ploidy: Int) {
+        val alleles = arrayOf(
+            "A", "C", "G", "T", "N",
+            "A", "C", "G", "T", "N",
+            "A", "C", "G", "T", "N",
+            "A", "C", "G", "T", "N",
+            "", "", "",
+            "AC", "ACG", "ACGT", "ACGTA", "ACGTAC", "ACGTACG", "ACGTACGT"
+        )
+    }
+
+    fun getTransposedFileStream(file: File): Stream<String> {
         var reader = BufferedReader(FileReader(file))
         val nRow = reader.readLine().split("\t".toRegex()).toTypedArray().size
         val nCol = reader.lines().count().toInt() + 1
@@ -148,17 +127,8 @@ class TempFiles2(verbose: Boolean = false) {
 //                    .collect(Collectors.toList());
     }
 
-    fun createSampleFastInputFile() {
-        val writer = BufferedWriter(FileWriter(sampleFastInputFile))
-        getTransposedFileStream(inputFile).forEach { line: String? ->
-            try {
-                writer.append(line)
-                writer.append(System.lineSeparator())
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
-        writer.close()
+    fun createSampleFastInputFile(inputFile: File = this.inputFile, outputFile: File = this.sampleFastInputFile) {
+        outputFile.bufferedWriter().use { getTransposedFileStream(inputFile).forEach(it::appendLine) }
     }
 
     fun createSampleFastEncodedFile() {
@@ -169,21 +139,12 @@ class TempFiles2(verbose: Boolean = false) {
         length: Int,
         alleles: Array<String>,
         ploidy: Int,
-        elementSeparator: String?,
-        alleleSeparator: String?
-    ): String {
-        val row = StringJoiner(elementSeparator)
-        val random = Random()
-        val n = alleles.size
-        for (i in 0 until length) {
-            val gt = StringJoiner(alleleSeparator)
-            for (j in 0 until ploidy) {
-                gt.add(alleles[random.nextInt(n)])
-            }
-            row.merge(gt)
-        }
-        return row.toString()
-    }
+        elementSeparator: String,
+        alleleSeparator: String
+    ) = (1..length).joinToString(elementSeparator) { randomGenotype(ploidy, alleles, alleleSeparator) }
+
+    fun randomGenotype(ploidy: Int, alleles: Array<String>, alleleSeparator: String) =
+        (1..ploidy).joinToString(alleleSeparator) { alleles[random.nextInt(alleles.size)] }
 
     companion object {
         fun transposeEncodedFile(inputFile: File, outputFile: File) {
@@ -229,6 +190,12 @@ class TempFiles2(verbose: Boolean = false) {
                     outputStream.write(10)
                 }
             }
+        }
+
+        private val alleles         by lazy {
+            listOf("A", "C", "G", "T").run {
+                flatMap { i -> flatMap { j -> flatMap { k -> listOf("$i$j$k", "$i$j${k}N") } } }
+            }.dropLast(1)
         }
     }
 
